3fb57d3b755ae572210f6b140cd9a8f4a731dfb5
Christoph Dwertmann <christoph.dwertmann@nicta.com.au>
2010-02-17 08:09:02 +0000

    added support for building AM and EC rpms

--- build_rpms.sh
+++ build_rpms.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 TOPDIR=`pwd`
 BUILD='debuild -uc -us -b | grep "dpkg-deb: building package" | awk -F "../" "{print substr(\$2,0,length(\$2)-2)}"'
 
@@ -11,14 +13,50 @@ function build {
 	done	
 }
 
-for i in external/frisbee/imagezip external/log4r-1.0.5 external/xmpp4r-0.4 omf-common ; do
+for i in external/frisbee/imagezip external/frisbee/frisbee external/coderay-0.8.3 external/log4r-1.0.5 external/xmpp4r-0.4 omf-common ; do
 	cd $i
 	build
 	cd $TOPDIR
 done
 
+# OMF aggmgr
+cd omf-aggmgr
+DEB=`bash -c "$BUILD"`
+echo "Converting $DEB to rpm"
+RPM=`sudo alien -r -g ../$DEB | grep Directory | awk -F ' ' '{print $2}'`
+if [ x$RPM == x ]; then
+	echo "Error generating RPM package in `pwd`. Exiting."
+	exit
+fi
+sudo rm -rf `pwd`/$RPM/etc/init.d
+sudo mkdir -p $RPM/etc/rc.d/init.d
+sudo cp ../omf-aggmgr/debian/init.d.fedora $RPM/etc/rc.d/init.d/omf-aggmgr-5.3
+sudo chmod +x $RPM/etc/rc.d/init.d/omf-aggmgr-5.3
+sudo sed -i 's/etc\/init.d/etc\/rc.d\/init.d/g' $RPM/$RPM*.spec
+sudo sed -i '/^Group: /a Requires: ruby frisbee libmysql-ruby, libldap-ruby1.8 libsqlite3-ruby psmisc nmap netcat liblog4r-ruby1.8 libxmpp4r-ruby1.8 omf-common-5.3' $RPM/$RPM*.spec
+sudo sed -i '/^(Converted /a %post\n/sbin/chkconfig --add omf-aggmgr-5.3\n/etc/init.d/omf-aggmgr-5.3 restart' $RPM/$RPM*.spec
+sudo rpmbuild -bb --target noarch-none-linux --buildroot `pwd`/$RPM $RPM/omf-aggmgr-*.spec
+sudo rm -rf `pwd`/$RPM
+cd $TOPDIR
+
+# OMF expctl
+cd omf-expctl
+DEB=`bash -c "$BUILD"`
+echo "Converting $DEB to rpm"
+RPM=`sudo alien -r -g ../$DEB | grep Directory | awk -F ' ' '{print $2}'`
+if [ x$RPM == x ]; then
+	echo "Error generating RPM package in `pwd`. Exiting."
+	exit
+fi
+sudo sed -i '/^Group: /a Requires: ruby liblog4r-ruby1.8 libxmpp4r-ruby1.8 omf-common-5.3 libcoderay-ruby1.8 libmarkaby-ruby' $RPM/$RPM*.spec
+sudo rpmbuild -bb --target noarch-none-linux --buildroot `pwd`/$RPM $RPM/omf-expctl-*.spec
+sudo rm -rf `pwd`/$RPM
+cd $TOPDIR
+
+# OMF resctl
 cd omf-resctl
 DEB=`bash -c "$BUILD"`
+echo "Converting $DEB to rpm"
 RPM=`sudo alien -r -g ../$DEB | grep Directory | awk -F ' ' '{print $2}'`
 if [ x$RPM == x ]; then
 	echo "Error generating RPM package in `pwd`. Exiting."
@@ -29,9 +67,9 @@ sudo mkdir -p $RPM/etc/rc.d/init.d
 sudo cp ../omf-resctl/debian/init.d.fedora $RPM/etc/rc.d/init.d/omf-resctl-5.3
 sudo chmod +x $RPM/etc/rc.d/init.d/omf-resctl-5.3
 sudo sed -i 's/etc\/init.d/etc\/rc.d\/init.d/g' $RPM/$RPM*.spec
-sudo sed -i '/^Group: /a Requires: ruby wireless-tools wget pciutils imagezip liblog4r-ruby1.8 libxmpp4r-ruby1.8 omf-common-5.3 omf-resctl-5.3' $RPM/$RPM*.spec
+sudo sed -i '/^Group: /a Requires: ruby wireless-tools wget pciutils imagezip liblog4r-ruby1.8 libxmpp4r-ruby1.8 omf-common-5.3' $RPM/$RPM*.spec
 sudo sed -i '/^(Converted /a %post\n/sbin/chkconfig --add omf-resctl-5.3\n/etc/init.d/omf-resctl-5.3 restart' $RPM/$RPM*.spec
-sudo rpmbuild -bb --buildroot `pwd`/$RPM $RPM/omf-resctl-*.spec
+sudo rpmbuild -bb --target noarch-none-linux --buildroot `pwd`/$RPM $RPM/omf-resctl-*.spec
 sudo rm -rf `pwd`/$RPM
 cd $TOPDIR
 
