8786361da2cc310da877f2a0bca608728b867ef6
Thierry Rakotoarivelo <thierry.rakotoarivelo@nicta.com.au>
2010-02-22 03:15:00 +0000

    Testing interaction RM-RC

--- omf-resmgr/ruby/omf-resmgr/managerCommands.rb
+++ omf-resmgr/ruby/omf-resmgr/managerCommands.rb
@@ -55,10 +55,21 @@ module AgentCommands
     commAddress = cmdObject.commaddr
     commUser = cmdObject.commuser
     commPassword = cmdObject.commpwd
+    
     # Create the sliver ...
-    #success = false
-    success = true
-    #....
+    success = false
+    if (agent.instance.config[:manager][:sliver] != nil) && 
+       (sliverCmd = agent.instance.config[:manager][:sliver][sliverType.to_sym] != nil) 
+      sliverCmd = sliverCmd.gsub(/%SLICE_NAME%/, sliceName)
+      sliverCmd = sliverCmd.gsub(/%RESOURCE_NAME%/, resourceName)
+      sliverCmd = sliverCmd.gsub(/%PUBSUB_ADDRESS%/, commAddress)
+      sliverCmd = sliverCmd.gsub(/%PUBSUB_USER%/, commUser)
+      sliverCmd = sliverCmd.gsub(/%PUBSUB_PASSWORD%/, commPassword)
+      MObject.debug("Creating a '#{sliverType}' with cmd: '#{sliverCmd}'") 
+      sliverID = "#{sliceName}-#{resourceName}"
+      ExecApp.new(sliverID, agent, sliverCmd)
+      success = true
+    end
 
     # Reply to the command
     if success  
@@ -83,16 +94,14 @@ module AgentCommands
   def AgentCommands.DELETE_SLIVER(agent, cmdObject)
     sliceName = cmdObject.slicename
     resourceName = cmdObject.resname
+    
     # delete the sliver ...
-    #success = false
-    success = true
-    #....
-
-    # Reply to the command
-    if success  
+    sliverID = "#{sliceName}-#{resourceName}"
+    begin
+      ExecApp[sliverID].kill(sliverID, 15)
       msg = "Deleted Sliver for '#{sliceName}' on '#{resourceName}'"
       agent.okReply(msg, cmdObject)
-    else
+    rescue
       msg = "Failed deleteing Sliver for '#{sliceName}' on '#{resourceName}'"
       agent.errorReply(msg, cmdObject)
     end
