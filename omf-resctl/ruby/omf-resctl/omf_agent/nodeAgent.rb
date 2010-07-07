7278d5db307be84c0e91554fec992d4ab45fa5e0
Thierry Rakotoarivelo <thierry.rakotoarivelo@nicta.com.au>
2010-07-07 03:57:15 +0000

    Fixed same problem as in r529 but on the RC side now - see log comment for r529

--- omf-resctl/ruby/omf-resctl/omf_agent/nodeAgent.rb
+++ omf-resctl/ruby/omf-resctl/omf_agent/nodeAgent.rb
@@ -389,24 +389,23 @@ class NodeAgent < MObject
     end
 
     kl = nil
-    if @config[:communicator][:authenticate_messages]
-      MObject.info("Message authentication is enabled")
-      if @config[:communicator][:private_key] == nil
-        error "No private key file specified on command line or config file! Exiting now!\n"
-	      exit
-      end
-      if @config[:communicator][:public_key_dir] == nil
-        error "No public key directory specified on command line or config file! Exiting now!\n"
-	      exit
-      end
-      kl = OMF::Security::KeyLocator.new(@config[:communicator][:private_key], @config[:communicator][:public_key_dir])
+    aflag = @config[:communicator][:authenticate_messages] 
+    if aflag
+      info "Message authentication is enabled"
+      raise "No private key file specified on command line or config file!" \
+            if !@config[:communicator][:private_key]
+      raise "No public key directory specified on command line or config " \
+            if !@config[:communicator][:public_key_dir]
+      kl = OMF::Security::KeyLocator.new(
+                            @config[:communicator][:private_key], 
+                            @config[:communicator][:public_key_dir])
     else
-      MObject.info("Message authentication is disabled")
+      info "Message authentication is disabled"
     end
 
-    ## initialize message envelope generator here with kl and authenticate_messages
-    OMF::Envelope.init(:authenticate_messages => authenticate_messages,
-                       :key_locator => kl)
+    # initialize message envelope generator here with kl and 
+    # authenticate_messages
+    OMF::Envelope.init(:authenticate_messages => afalg, :key_locator => kl)
   end
 
   ################################################
