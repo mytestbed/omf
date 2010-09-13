8e24687d82ffcf6e859dfa4210c39b8117ea769a
Thierry Rakotoarivelo <thierry.rakotoarivelo@nicta.com.au>
2010-09-13 08:04:37 +0000

    Updated the PackageMaker project for OSX build We now have an OSX package for the EC OMF 5.3

--- omf-expctl/osx/post-install.sh
+++ omf-expctl/osx/post-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 VERSION="5.3"
+REVISION="trunk"
 INDIR="/tmp/omf-install-tmp/"
 
 # Ruby of EC
@@ -22,6 +23,7 @@ cp $INDIR/omf-expctl/bin/omf /usr/bin/omf-$VERSION
 
 # Common
 mv $INDIR/omf-common/ruby /usr/share/omf-common-$VERSION
+mv $INDIR/omf-common/share /usr/share/omf-common-$VERSION
 chown -R root /usr/share/omf-common-$VERSION
 chgrp -R admin /usr/share/omf-common-$VERSION
 
@@ -35,6 +37,17 @@ mv $INDIR/external/log4r-1.0.5/src/* /usr/lib/ruby/1.8/
 chown -R root $INDIR/external/xmpp4r-0.4
 chgrp -R admin $INDIR/external/xmpp4r-0.4
 mv $INDIR/external/xmpp4r-0.4/lib/* /usr/lib/ruby/1.8/
+
+# Tools
+mv $INDIR/tools /usr/share/omf-expctl-$VERSION
+chown -R root /usr/share/omf-expctl-$VERSION/tools
+chgrp -R admin /usr/share/omf-expctl-$VERSION/tools
+
+# Install the JSON Gem
+/usr/bin/gem install json
+
+# Set the REVISION (should find a better way to do this)
+echo $REVISION >/usr/share/omf-expctl-$VERSION/omf-expctl/REVISION
 
 # Clean up
 rm -rf $INDIR
