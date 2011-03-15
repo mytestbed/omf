1b4a09ea53dc0820937629fcd30af7d696d45e76
Christoph Dwertmann <christoph.dwertmann@nicta.com.au>
2011-03-15 11:56:26 +1100

    added support for fqdn placeholder to the RC

--- omf-resctl/ruby/omf-resctl/omf_agent/nodeAgent.rb
+++ omf-resctl/ruby/omf-resctl/omf_agent/nodeAgent.rb
@@ -359,6 +359,7 @@ class NodeAgent < MObject
     end
     # substitute hostname or mac addr, if required
     @config[:agent][:name].gsub!(/%hostname%/, `/bin/hostname`.chomp)
+    @config[:agent][:name].gsub!(/%fqdn%/, `/bin/hostname --fqdn`.chomp)
     @config[:agent][:name].gsub!(/%macaddr%/, `ifconfig #{@config[:communicator][:control_if]} | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'`.chomp)
     @agentName = @config[:agent][:name] 
     @agentSlice =  @config[:agent][:slice] 
