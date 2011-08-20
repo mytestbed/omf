dfd13ae9dfe6c2e2d551d54ed09e88c5dfb232c7
Max Ott <max.ott@nicta.com.au>
2011-08-21 05:37:25 +1000

    Not sure why this is necessary

--- omf-web/ruby/web2/tab/log/init.rb
+++
@@ -1,9 +0,0 @@
-require 'omf-common/web2/tab/log/log_service'
-register_tab(
-    :id => :log,
-    :name => 'Log', 
-    :priority => 900, 
-    :def_enabled => true, 
-    :class => OMF::Common::Web2::Log::LogService
-)
-
