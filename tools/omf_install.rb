96e664095901fd09ce5be404bce44d8f8e035a89
Christoph Dwertmann <christoph.dwertmann@nicta.com.au>
2012-01-18 08:01:12 +0000

    second iteration of OMF install script

--- tools/omf_install.rb
+++ tools/omf_install.rb
@@ -10,9 +10,7 @@ require 'yaml'
 
 @c= YAML::load( File.open( 'omf_install.yaml' ) )
 
-p @c
-
-
+@pxe_url = "http://pkg.mytestbed.net/files/#{@c['version']}/pxe/"
 
 class String
   def red; colorize(self, "\e[1m\e[31m"); end
@@ -26,15 +24,21 @@ class String
 end
 
 def ok
-  puts "[ "+"OK".green+" ]"
+  puts "\t[ "+"OK".green+" ]"
 end
 
 def failed
-  puts "[ "+"FAILED".red+" ]"
+  puts "\t[ "+"FAILED".red+" ]"
 end
 
 def task(text)
-  print "---> ".pur+text+"\t"
+  print "---> ".pur+text
+end
+
+def replace_in_file(file, pattern, replacement)
+  text = File.read(file)
+  text.gsub!(/#{pattern}/,replacement)
+  File.open(file, "w") {|f| f.write text}
 end
 
 def check_root
@@ -102,6 +106,93 @@ EOF
       cnt+=1 
     }
   }
+  if system("/etc/init.d/dnsmasq restart")
+    ok
+  else
+    failed
+    abort "Failed to start dnsmasq. Please check the configuration files "+
+      "manually. If you ran the OMF installer before, remove the OMF section "+
+      "from /etc/dnsmasq.conf"
+  end
+end
+
+def config_pxe
+  task "Configuring PXE booting\n"
+  system("mkdir -p /tftpboot/pxelinux.cfg; ln -s /usr/lib/syslinux/pxelinux.0 /tftpboot/")
+  system("wget -N #{@pxe_url}#{@c['kernel_name']} -P /tftpboot")
+  system("wget -N #{@pxe_url}#{@c['initramfs_name']} -P /tftpboot")
+  File.open("/tftpboot/pxelinux.cfg/pxeconfig", 'w') {|f| 
+    contents = <<EOF
+SERIAL 0 19200 0
+DEFAULT linux
+LABEL linux
+KERNEL #{@c['kernel_name']}
+APPEND console=tty0 console=ttyS0,19200n8 vga=normal quiet root=/dev/ram0 rw load_ramdisk=1 prompt_ramdisk=1 ramdisk_size=32768 initrd=#{@c['initramfs_name']} control=#{@c['interface']} xmpp=#{@c['xmpp_domain']} slice=pxe_slice hrn=#{@c['hrn']} 
+PROMPT 0
+EOF
+    f.write(contents)
+  }
+  File.open("/tftpboot/pxelinux.cfg/default", 'w') {|f| 
+  contents = <<EOF
+DEFAULT harddrive
+LABEL harddrive
+localboot 0
+EOF
+    f.write(contents)
+  }
+  ok
+end
+
+def install_xmpp
+  task "Installing XMPP server"
+  system("add-apt-repository ppa:ferramroberto/java; apt-get update; apt-get -y install sun-java6-jre")
+  system("wget -N -P /tmp wget http://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_3.7.1_all.deb")
+  system("dpkg -i /tmp/downloadServlet?filename=openfire%2Fopenfire_3.7.1_all.deb")
+  ok
+end
+
+def config_xmpp
+  task "Configuring XMPP server\n\n"
+  contents = <<EOF
+* direct your web browser to 'http://#{@c['controller_ip']}:9090' and begin the setup wizard
+* choose your language and click continue
+* enter '#{@c['xmpp_domain']}' in the Domain field and click continue
+* choose the embedded database. You can also use mysql, but you will have to create a mysql user and a database manually first.
+* choose the default profile and click continue
+* enter an admin password and click continue, then wait until the installation is finished
+* log on to the web GUI at 'http://#{@c['controller_ip']}:9090' with the user/password you set in the wizard    
+EOF
+  puts contents
+  puts "\nPlease follow the steps above. After you've set up Openfire, press any key to continue the OMF installation.".red  
+  gets
+  ok
+end
+
+def install_am
+  task "Installing Aggregate Manager"
+  File.open("/etc/apt/sources.list.d/omf.list", 'w') {|f| 
+    f.write("deb http://pkg.mytestbed.net/ubuntu oneiric/")
+  }
+  system("apt-get update; apt-get install -y --allow-unauthenticated omf-aggmgr-5.4 oml2-server")
+  ok
+end
+
+def config_am
+  task "Configuring Aggregate Manager\n"
+  system("cp /usr/share/doc/omf-aggmgr-5.4/examples/omf-aggmgr.yaml /etc/omf-aggmgr-5.4/")
+  cmd = <<EOF
+cd /etc/omf-aggmgr-5.4/enabled
+ln -s ../available/cmcStub.yaml
+ln -s ../available/frisbee.yaml
+ln -s ../available/pxe.yaml
+ln -s ../available/inventory.yaml
+ln -s ../available/result.yaml
+ln -s ../available/saveimage.yaml
+EOF
+  system(cmd)
+  replace_in_file("/etc/omf-aggmgr-5.4/omf-aggmgr.yaml","  :server: \"norbit.npc.nicta.com.au\"","  :server: \"#{@c['xmpp_domain']}\"")
+  replace_in_file("/etc/omf-aggmgr-5.4/available/saveimage.yaml","      saveimageIF: 10.0.0.200","      saveimageIF: #{@c['controller_ip']}")
+  replace_in_file("/etc/omf-aggmgr-5.4/available/frisbee.yaml","      multicastIF: 10.0.0.200","      multicastIF: #{@c['controller_ip']}")
   ok
 end
 
@@ -111,5 +202,11 @@ puts "---> ".pur + "Please report any issues to "+"omf-user@lists.nicta.com.au".
 check_root
 check_ubuntu_version
 check_interface
-install_packages("syslinux dnsmasq ntp wget")
-config_dnsmasq
+#install_packages("syslinux dnsmasq ntp wget python-software-properties")
+#config_dnsmasq
+#config_pxe
+#install_xmpp
+#config_xmpp
+#install_am
+config_am
+install_packages("mysql-server libdb4.6 phpmyadmin")
