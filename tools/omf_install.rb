74eb0d346084e83d9d0525344752a811919878a1
Christoph Dwertmann <christoph.dwertmann@nicta.com.au>
2012-01-20 06:52:36 +0000

    completed OMF installer, needs testing

--- tools/omf_install.rb
+++ tools/omf_install.rb
@@ -8,10 +8,6 @@
 
 require 'yaml'
 
-@c= YAML::load( File.open( 'omf_install.yaml' ) )
-
-@pxe_url = "http://pkg.mytestbed.net/files/#{@c['version']}/pxe/"
-
 class String
   def red; colorize(self, "\e[1m\e[31m"); end
   def green; colorize(self, "\e[1m\e[32m"); end
@@ -21,6 +17,10 @@ class String
   def dark_blue; colorize(self, "\e[34m"); end
   def pur; colorize(self, "\e[1m\e[35m"); end
   def colorize(text, color_code)  "#{color_code}#{text}\e[0m" end
+  def prune_comma
+    self.chomp!(",\n")
+    self << ";\n"
+  end
 end
 
 def ok
@@ -41,8 +41,12 @@ def replace_in_file(file, pattern, replacement)
   File.open(file, "w") {|f| f.write text}
 end
 
+def get_hrn(n)
+  @c["hrn"].gsub(/%hostname%/,n)
+end
+
 def check_root
-  task("Checking User Name")
+  task("Checking for root privileges")
   if `whoami` == "root\n"
     ok
   else
@@ -64,12 +68,43 @@ end
 def check_interface
   task "Checking Network Interface #{@c['interface']}"
   i = `ifconfig #{@c['interface']}`
-  if i.include? "10.0.0.191" or i.include? "UP"
+  if i.include? @c['controller_ip'] and i.include? "UP"
     ok
   else
     failed
     abort "Please configure the node-facing network interface (#{@c['interface']}) "+ 
-    "to use the IP address 10.0.0.200 with netmask 255.255.255.0"
+    "to use the IP address #{@c['controller_ip']} and ensure that it's active"
+  end
+end
+
+def check_config
+  task "Checking configuration file"
+  begin
+    @c=YAML::load( File.open( 'omf_install.yaml' ) )
+  rescue
+    failed
+    abort "Error loading the omf_install.yaml configuration file. Make sure it" + 
+      " exists in the current directory and has valid YAML syntax."
+  end
+  @pxe_url = "http://pkg.mytestbed.net/files/#{@c['version']}/pxe/"
+  ok
+end
+
+def warning
+  task "WARNING\n".red
+  contents = <<EOF
+This script should only be used for a fresh OMF installation. If OMF was 
+previously installed on this system, existing configuration files and
+database tables will be overwritten. 
+
+Before running this installer you should define your testbed parameters by
+editing the file 'omf_installer.yaml' in this directory.
+
+Are you sure you want to continue? (y/N) 
+EOF
+  print contents.red
+  if gets.downcase != "y\n"
+    abort "Installation stopped"
   end
 end
 
@@ -102,7 +137,7 @@ EOF
   File.open("/etc/dnsmasq.d/omf_testbed.conf", 'w') {|f| 
     cnt = 1
     @c['macs'].each {|m|
-      f.puts("dhcp-host=net:control,#{m},node#{cnt},#{@c['address_pool_prefix']}#{cnt}")
+      f.puts("dhcp-host=net:control,#{m},node#{cnt},#{@c['address_pool_prefix']}#{cnt+@c['address_pool_start']-1}")
       cnt+=1 
     }
   }
@@ -156,11 +191,11 @@ def config_xmpp
   contents = <<EOF
 * direct your web browser to 'http://#{@c['controller_ip']}:9090' and begin the setup wizard
 * choose your language and click continue
-* enter '#{@c['xmpp_domain']}' in the Domain field and click continue
-* choose the embedded database. You can also use mysql, but you will have to create a mysql user and a database manually first.
+* enter '#{@c['xmpp_domain']}' in the domain field and click continue
+* choose the embedded database. You can also use mySQL, but you will have to install mySQL and create a mySQL user and a database manually first.
 * choose the default profile and click continue
 * enter an admin password and click continue, then wait until the installation is finished
-* log on to the web GUI at 'http://#{@c['controller_ip']}:9090' with the user/password you set in the wizard    
+* log on to the web GUI at 'http://#{@c['controller_ip']}:9090' with the user/password you set in the wizard and check your settings
 EOF
   puts contents
   puts "\nPlease follow the steps above. After you've set up Openfire, press any key to continue the OMF installation.".red  
@@ -196,17 +231,103 @@ EOF
   ok
 end
 
-puts "---> ".pur + "OMF #{@c['version']} Installation Script".dark_green
-puts "---> ".pur + "Please report any issues to "+"omf-user@lists.nicta.com.au".red
+def config_inventory
+  task "Configuring Inventory\n"
+  system("mysql --defaults-file=/etc/mysql/debian.cnf << EOF
+drop database if exists inventory;
+create database inventory;
+GRANT ALL PRIVILEGES  ON inventory.* 
+TO 'omf'@'localhost' IDENTIFIED BY 'omf' 
+WITH GRANT OPTION;
+EOF
+")
+  system("mysql -u omf -pomf << EOF
+use inventory;
+flush tables;
+EOF
+")
+  system("zcat /usr/share/doc/omf-aggmgr-#{@c['version']}/examples/inventory.sql.gz | mysql -u omf -pomf inventory")
+  
+  devices = ""
+  locations = ""
+  motherboards = ""
+  nodes = ""
+  cnt = 1
+  @c['macs'].each {|m|
+    devices << "(#{cnt}, 1, #{cnt}, 1, 'Bogus 00:01', '#{m}', 'control'),\n"
+    locations << "(#{cnt}, 'location name', 1, #{cnt}, 1, NULL, NULL, NULL, 1),\n"
+    motherboards << "(#{cnt}, 1, NULL, NULL, 1, NULL, NULL, NULL, 1, NULL),\n"
+    nodes << "(#{cnt}, '#{@c['address_pool_prefix']}#{cnt+@c['address_pool_start']-1}', '#{m}', 'node#{cnt}', '#{get_hrn("node#{cnt}")}', 1, 'BOGUS SN 123', #{cnt}, #{cnt}, 1, '/dev/sda'),\n"
+    cnt+=1 
+  }
+  
+  system("mysql -u omf -pomf << EOF
+use inventory;
+truncate devices;
+INSERT INTO devices (id, device_kind_id, motherboard_id, inventory_id, address, mac, canonical_name) VALUES
+#{devices.prune_comma}
+truncate locations;
+INSERT INTO locations (id, name, x, y, z, latitude, longitude, elevation, testbed_id) VALUES
+#{locations.prune_comma}
+truncate motherboards;
+INSERT INTO motherboards (id, inventory_id, mfr_sn, cpu_type, cpu_n, cpu_hz, hd_sn, hd_size, hd_status, memory) VALUES
+#{motherboards.prune_comma}
+truncate nodes;
+INSERT INTO nodes (id, control_ip, control_mac, hostname, hrn, inventory_id, chassis_sn, motherboard_id, location_id, pxeimage_id, disk) VALUES
+#{nodes.prune_comma}
+EOF
+")
+end
 
+def config_psnodes
+  task "Creating pubsub nodes"
+  system("omf_create_psnode-#{@c['version']} #{@c['xmpp_domain']} mksys")
+  nodes = ""
+  cnt = 1
+  @c['macs'].each {|m|
+    nodes << "#{get_hrn("node#{cnt}")} "
+    cnt+=1
+  }
+  system("omf_create_psnode-#{@c['version']} #{@c['xmpp_domain']} mkslice pxe_slice #{nodes}")
+  system("omf_create_psnode-#{@c['version']} #{@c['xmpp_domain']} mkslice default_slice #{nodes}")
+end
+
+def restart_am
+  task "Restarting AM\n"
+  system("/etc/init.d/omf-aggmgr-#{@c['version']} restart")
+end
+
+task "OMF Installation Script\n".dark_green
+task "Please report any issues to "+"omf-user@lists.nicta.com.au\n".red
+
+check_config
 check_root
 check_ubuntu_version
 check_interface
-#install_packages("syslinux dnsmasq ntp wget python-software-properties")
-#config_dnsmasq
-#config_pxe
-#install_xmpp
-#config_xmpp
-#install_am
+warning
+install_packages("syslinux dnsmasq ntp wget python-software-properties")
+config_dnsmasq
+config_pxe
+install_xmpp
+config_xmpp
+install_am
 config_am
 install_packages("mysql-server libdb4.6 phpmyadmin")
+config_inventory
+config_psnodes
+restart_am
+
+task "Done!\n".green
+puts <<EOF
+If there weren't any errors reported above, you now have the following OMF components running:
+
+* DHCP/DNS/TFTP server
+* PXE configuration + kernel + initramfs for network booting
+* XMPP server with default pubsub nodes
+* Aggregate Manager with default services enabled
+* pre-populated Inventory Database
+
+You should now be ready to run experiments on your testbed. Now please install the OMF 
+Experiment Controller according to the installation guide on this machine or another PC.
+
+EOF
