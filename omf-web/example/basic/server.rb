02ae21eeb7a0eee27c3f0d993617e0f095f1b9df
Jack C Hong <jhong@nicta.com.au>
2012-02-06 11:00:49 +1100

    Update server.rb to handle more flexible graph definition
    User can define arbitary tabs, rendered in multi-card template
    Can include data from different database table in one tab
    It will retrieve data based on table/column used accross tabs/widgets

--- omf-web/example/basic/server.rb
+++ omf-web/example/basic/server.rb
@@ -2,22 +2,160 @@
 
 require 'rubygems'
 require 'nokogiri'
+require 'ruby-debug'
 
 require 'net/http'
 require 'yaml'
 require 'ostruct'
 require 'optparse'
 
-require "omf-common/communicator/xmpp/omfPubSubTransport"
 require 'omf-oml/table'
-require 'omf-oml/sql_source'
+
 require 'omf-web/tabbed_server'
-require 'omf-web/tab/graph/init'
-require 'omf-web/widget/code/code'
+require 'omf-web/tab/code/code_service'
+require 'omf-web/tab/common/abstract_service'
+
+require 'omf-web/widget/abstract_data_widget'
 
 RESULT2_PATH = "/result2/query"
 RESULT2_NAMESPACE = "http://schema.mytestbed.net/am/result/2/"
 CURRENT_DIR = File.dirname(__FILE__)
+DEFAULT_TABS = %w(code log graph)
+
+# Create a custom tab type, this should not be neccessary if AbsctractService is generic enough
+#
+class BasicPage < OMF::Web::Tab::AbstractService
+  def initialize(tab_id, opts)
+    super
+    debug "New Basic Service: #{opts.keys.inspect}"
+    @widgets = (opts[:widgets] || []).map {|w| w[:widget_class].new(w) }
+    @tab_id = opts[:tab_id]
+  end
+
+  def show(req, opts)
+    opts[:card_id] = req.params['tid'].to_i rescue 0
+    widget = @widgets[opts[:card_id]]
+
+    opts[:widget] = widget
+    opts[:card_title] = widget.name
+
+    OMF::Web::Theme.require 'multi_card_page'
+
+    page = OMF::Web::Theme::MultiCardPage.new(widget, @tab_id, @widgets, opts)
+
+    [page.to_html, 'text/html']
+  end
+end
+
+# Monkeypatch String
+#
+class String
+  def auto_parse
+    Integer(self) rescue Float(self) rescue self
+  end
+end
+
+# Monkeypatch AbstractDataWidget
+#
+# This is due to some inconsistence of multi-card theme
+#
+module OMF::Web::Widget
+  class AbstractDataWidget
+    def [](key)
+      opts[key]
+    end
+  end
+end
+
+# Define visualisation widgets
+#
+def widget_definition(name, data_source, viz_type, mapping)
+  viz_opts = { :schema => data_source.schema }
+
+  case viz_type
+  when 'table'
+  when 'line_chart'
+    viz_opts[:mapping] = {
+      :x_axis => { :property => mapping['x'] },
+      :y_axis => { :property => mapping['y'] },
+      :group_by => { :property => mapping['group_by'] },
+      :stroke_width => 2
+    }
+  end
+
+  {
+    :name => name,
+    :data_sources => { :default => data_source },
+    :js_url => "graph/#{viz_type}.js",
+    :js_class => "OML.#{viz_type}",
+    :widget_class => OMF::Web::Widget::AbstractDataWidget,
+    :dynamic => false,
+    :viz_type => viz_type,
+    :wopts => viz_opts
+  }
+end
+
+# Add source code tab
+# Add all ruby files in the current directory
+#
+def code_tab(pattern)
+  Dir.glob(pattern).each do |filename|
+    OMF::Web::Widget::Code.addCode(
+      filename,
+      :file => "#{CURRENT_DIR}/#{filename}"
+    )
+  end
+end
+
+# Add custom tab
+#
+def custom_tab(tab_id, widgets)
+  OMF::Web::Tab.register_tab(
+    :id => tab_id,
+    :name => tab_id.to_s.capitalize,
+    :class => BasicPage,
+    :opts => { :tab_id => tab_id, :widgets => widgets }
+  )
+end
+
+# Request result2 service and get data rows
+#
+def result2_data_rows(experiment_id, uri, table, columns)
+  data_rows = []
+
+  req = Net::HTTP::Post.new(uri.path)
+
+  query = Nokogiri::XML::Builder.new do |xml|
+    xml.request(:id => 'foo') {
+      xml.result { xml.format 'xml' }
+      xml.query {
+        xml.repository(:name => experiment_id)
+        xml.table(:tname => table)
+        xml.project {
+          columns.each do |m|
+            xml.arg { xml.col(:name => m.to_s, :table => table) }
+          end
+        }
+      }
+    }
+  end.to_xml
+
+  req.body = query
+  result = Net::HTTP.start(uri.host, uri.port) do |http|
+    res = http.request(req)
+    res.body
+  end
+
+  Nokogiri::XML(result).xpath('//omf:r', 'omf'=> RESULT2_NAMESPACE).each do |r|
+    row = r.xpath('omf:c', 'omf'=> RESULT2_NAMESPACE).map {|v| v.content.auto_parse }
+    data_rows << row
+  end
+
+  data_rows
+end
+
+# Parse options
+#
 
 @options = OpenStruct.new
 
@@ -36,119 +174,51 @@ raise "Missing experiment id" if ARGV[0].nil?
 
 @experiment_id = ARGV[0]
 @config = YAML::load_file(@options.config)
-@results = []
-@result2_uri = URI(@config['data']['result2_server'] + RESULT2_PATH)
-@tables = @config['data']['tables']
-@mappings = @config['data']['mappings']
+
 @tabs = @config['tabs']
+@result2_uri = URI(@config['result2_server'] + RESULT2_PATH)
+@results = []
 
-# Monkeypatch String
-#
-class String
-  def auto_parse
-    Integer(self) rescue Float(self) rescue self
-  end
-end
+custom_tabs = @tabs.keys - DEFAULT_TABS
 
-# Add graph widgets
-#
-def add_graph(name, data_source, viz_type, *column_names)
-  x, y, group = *(column_names.map {|v| v.to_sym})
-
-  viz_opts = {
-    :schema => data_source.schema,
-    :mapping => {
-      :x_axis => { :property => x },
-      :y_axis => { :property => y },
-      :group_by => { :property => group },
-      :stroke_width => 2
-    }
-  }
-
-  OMF::Web::Widget::Graph.addGraph("#{name} #{viz_type}", {
-                                    :data_sources => { :default => data_source },
-                                    :dynamic => true,
-                                    :viz_type => viz_type,
-                                    :wopts => viz_opts
-                                  })
-end
-
-# Add source code tab
-# Add all ruby files in the current directory
-#
-def add_code
-  Dir.glob("*.rb").each do |filename|
-    OMF::Web::Widget::Code.addCode(
-      filename,
-      :file => "#{CURRENT_DIR}/#{filename}"
-    )
-  end
-end
-
-def result2_data_rows(experiment_id, uri, tables, mappings)
-  results = []
-  data_rows = []
-
-  req = Net::HTTP::Post.new(uri.path)
-
-  result2_queries = tables.map do |table|
-    Nokogiri::XML::Builder.new do |xml|
-      xml.request(:id => 'foo') {
-        xml.result { xml.format 'xml' }
-        xml.query {
-          xml.repository(:name => experiment_id)
-          xml.table(:tname => table)
-          xml.project {
-            mappings['columns'].each do |m|
-              xml.arg { xml.col(:name => m['name'], :table => table) }
-            end
-          }
-        }
-      }
-    end.to_xml
-  end
-
-  result2_queries.each do |result_query|
-    req.body = result_query
-    Net::HTTP.start(uri.host, uri.port) do |http|
-      res = http.request(req)
-      results << res.body
+data_tables = {}.tap do |hash|
+  custom_tabs.each do |tab_key|
+    @tabs[tab_key].each do |widget|
+      hash[widget['data']] ||= []
+      hash[widget['data']] << (widget['mapping'] && widget['mapping'].values)
+      hash[widget['data']]
     end
   end
+end
 
-  results.each do |result|
-    Nokogiri::XML(result).xpath('//omf:r', 'omf'=> RESULT2_NAMESPACE).each do |r|
-      row = r.xpath('omf:c', 'omf'=> RESULT2_NAMESPACE).map {|v| v.content.auto_parse }
-      data_rows << row
+data_tables.values.each { |v| v = v.compact!}
+data_tables.values.each { |v| v = v.flatten!}
+data_tables.values.each { |v| v = v.uniq!}
+
+@data_sources = {}.tap do |hash|
+  data_tables.keys.each do |t|
+    schema = data_tables[t].map {|v| [v.to_sym, :text]}
+    data_source = OMF::OML::OmlTable.new(@experiment_id, schema)
+
+    result2_data_rows(@experiment_id, @result2_uri, t, data_tables[t]).each do |row|
+      data_source.add_row(row)
     end
-  end
-
-  data_rows.group_by {|r| r[0]}
-end
-
-# Configure data source
-#
-schema = @mappings['columns'].map {|v| [v['name'].to_sym, v['type'].to_sym]}
-@data_source = OMF::OML::OmlTable.new(@experiment_id, schema, :index => @mappings['index'])
-
-grouped_rows = result2_data_rows(@experiment_id, @result2_uri, @tables, @mappings)
-
-grouped_rows.keys.sort.each do |key|
-  grouped_rows[key].each do |row|
-    @data_source.add_row(row)
+    hash[t] ||= data_source
   end
 end
 
-@tabs['graph']['widgets'].each do |widget|
-  add_graph(@tabs['graph']['title'], @data_source, widget, *(@mappings['columns'].map {|v| v['name']}))
+custom_tabs.each do |tab_id|
+  widgets = @tabs[tab_id].map do |w|
+    widget_definition(w['name'], @data_sources[w['data']], w['type'], w['mapping'])
+  end
+  custom_tab(tab_id.to_sym, widgets)
 end
 
-add_code
+code_tab(@tabs['code']['pattern'])
 
 OMF::Web.start({
-  :page_title => @config['title'],
-  :use_tabs => @tabs.keys.map {|v| v.to_sym},
+  :port => @config['port'] || 4040,
+  :page_title => @config['title'] || @experiment_id,
+  :use_tabs => @config['tabs'].keys.map{|v| v.to_sym},
   :theme => 'bright'
 })
-
-
