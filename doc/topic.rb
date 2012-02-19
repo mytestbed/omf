ad3e98cb31e9285ec0860fb79fd507d97cac7ad2
Max Ott <max.ott@nicta.com.au>
2012-02-20 09:55:49 +1100

    Some initial design drafts for the OMF6

---
+++ doc/topic.rb
@@ -0,0 +1,42 @@
+
+
+module OMF::Communication
+
+  
+  # ???? 
+  class Topic
+    # Creates a new topic whose name is scoped in this topic. If +block+
+    #is provided, also subscribe to this topic 
+    #
+    def create(topic_name, &onMessage) # string, block => Topic
+    end
+    
+    # Returns a topic instance if it already exists in PubSub service, 
+    #  otherwise throw exception. If +block+
+    #  is provided, also subscribe to this topic 
+    def get(topic_name, &onMessage) # string, block => Topic;
+    end
+
+    # Subscribe to +topic+ and process any incoming message with +block+. 
+    #
+    def subscribe(&onMessage) # block
+    end
+    
+    # Unsubscribe from this +topic+ 
+    #
+    def unsubscribe()
+    end
+    
+    # Release this topic
+    #
+    def release()
+    end
+    
+    # Call +block+ for any error occuring in the execution of any of the above commands
+    #
+    def on_error(onError) # block 
+    end
+    
+  end # Topic
+
+end
