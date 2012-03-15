d58c57c4ccbc5e4ef6e6ffcb72cd30fa7fa7fbc5
Christoph Dwertmann <christoph.dwertmann@nicta.com.au>
2012-03-15 16:55:53 +1100

    modified RPM build script for 5.4

--- build_rpms.sh
+++ build_rpms.sh
@@ -14,7 +14,7 @@ function build {
 	done	
 }
 
-for i in external/frisbee/imagezip external/frisbee/frisbee external/coderay-0.8.3 external/log4r-1.0.5 external/xmpp4r-0.4; do
+for i in external/frisbee/imagezip external/frisbee/frisbee; do
 	cd $i
 	build
 	cd $TOPDIR
@@ -29,7 +29,8 @@ if [ x$RPM == x ]; then
 	echo "Error generating RPM package in `pwd`. Exiting."
 	exit
 fi
-sudo sed -i '/^(Converted /a %post\nln -s /usr/bin/ruby /usr/bin/ruby1.8' $RPM/$RPM*.spec
+sudo sed -i '/^(Converted /a %post\nln -s /usr/bin/ruby /usr/bin/ruby1.8\ncd /usr/share/omf-common-5.4/gems/1.8\n GEM_PATH=$PWD gem install --no-ri --no-rdoc -l -f cache/*.gem' $RPM/$RPM*.spec
+sudo sed -i '/^Group: /a Requires: ruby(abi) = 1.8 ruby-devel rubygems rubygem-rake' $RPM/$RPM*.spec
 sudo rpmbuild -bb --target noarch-none-linux --buildroot `pwd`/$RPM $RPM/omf-common-*.spec
 sudo rm -rf `pwd`/$RPM
 cd $TOPDIR
@@ -48,8 +49,8 @@ sudo mkdir -p $RPM/etc/rc.d/init.d
 sudo cp ../omf-aggmgr/debian/init.d.fedora $RPM/etc/rc.d/init.d/omf-aggmgr-5.4
 sudo chmod +x $RPM/etc/rc.d/init.d/omf-aggmgr-5.4
 sudo sed -i 's/etc\/init.d/etc\/rc.d\/init.d/g' $RPM/$RPM*.spec
-sudo sed -i '/^Group: /a Requires: ruby(abi) = 1.8 frisbee ruby-mysql, ruby-ldap ruby-sqlite3 psmisc nmap nc liblog4r-ruby1.8 libxmpp4r-ruby1.8 omf-common-5.4' $RPM/$RPM*.spec
-sudo sed -i '/^(Converted /a %post\n/sbin/chkconfig --add omf-aggmgr-5.4\n/etc/init.d/omf-aggmgr-5.4 restart' $RPM/$RPM*.spec
+sudo sed -i '/^Group: /a Requires: ruby(abi) = 1.8 frisbee ruby-mysql, ruby-ldap ruby-sqlite3 psmisc nmap nc mysql-devel omf-common-5.4' $RPM/$RPM*.spec
+sudo sed -i '/^(Converted /a %post\n/sbin/chkconfig --add omf-aggmgr-5.4\ncd /usr/share/omf-aggmgr-5.4/gems/1.8\n GEM_PATH=$PWD gem install --no-ri --no-rdoc -l -f cache/*.gem\n/etc/init.d/omf-aggmgr-5.4 restart' $RPM/$RPM*.spec
 sudo rpmbuild -bb --target noarch-none-linux --buildroot `pwd`/$RPM $RPM/omf-aggmgr-*.spec
 sudo rm -rf `pwd`/$RPM
 cd $TOPDIR
@@ -63,8 +64,9 @@ if [ x$RPM == x ]; then
 	echo "Error generating RPM package in `pwd`. Exiting."
 	exit
 fi
-sudo sed -i '/^Group: /a Requires: ruby(abi) = 1.8 liblog4r-ruby1.8 libxmpp4r-ruby1.8 omf-common-5.4 libcoderay-ruby1.8 rubygem-markaby' $RPM/$RPM*.spec
+sudo sed -i '/^Group: /a Requires: omf-common-5.4' $RPM/$RPM*.spec
 sudo sed -i '/conf_room_demo/d' $RPM/$RPM*.spec
+sudo sed -i '/^(Converted /a %post\ncd /usr/share/omf-expctl-5.4/gems/1.8\n GEM_PATH=$PWD gem install --no-ri --no-rdoc -l -f cache/*.gem' $RPM/$RPM*.spec
 sudo rpmbuild -bb --target noarch-none-linux --buildroot `pwd`/$RPM $RPM/omf-expctl-*.spec
 sudo rm -rf `pwd`/$RPM
 cd $TOPDIR
@@ -83,17 +85,15 @@ sudo mkdir -p $RPM/etc/rc.d/init.d
 sudo cp ../omf-resctl/debian/init.d.fedora $RPM/etc/rc.d/init.d/omf-resctl-5.4
 sudo chmod +x $RPM/etc/rc.d/init.d/omf-resctl-5.4
 sudo sed -i 's/etc\/init.d/etc\/rc.d\/init.d/g' $RPM/$RPM*.spec
-sudo sed -i '/^Group: /a Requires: ruby(abi) = 1.8 wireless-tools wget pciutils imagezip liblog4r-ruby1.8 libxmpp4r-ruby1.8 omf-common-5.4 util-linux-ng e2fsprogs' $RPM/$RPM*.spec
+sudo sed -i '/^Group: /a Requires: wireless-tools wget pciutils imagezip omf-common-5.4 util-linux-ng e2fsprogs' $RPM/$RPM*.spec
 sudo sed -i '/^(Converted /a %post\n/sbin/chkconfig --add omf-resctl-5.4\n/etc/init.d/omf-resctl-5.4 restart' $RPM/$RPM*.spec
 sudo rpmbuild -bb --target noarch-none-linux --buildroot `pwd`/$RPM $RPM/omf-resctl-*.spec
 sudo rm -rf `pwd`/$RPM
 cd $TOPDIR
 
-rm -f liblog4r-ruby-1*rpm libxmpp4r-ruby-1*rpm
-
 read -p "Do you want to upload the RPMs to mytestbed.net (y/n)?"
 if [ "$REPLY" == "y" ]; then
-	scp omf-*.rpm mytestbed.net:/var/www/packages/rpm/5.3/f14/noarch/
-	ssh mytestbed.net createrepo /var/www/packages/rpm/5.3/f14
-	ssh mytestbed.net createrepo /var/www/packages/rpm/5.3/f8
+	scp omf-*.rpm mytestbed.net:/var/www/packages/rpm/5.4/f14/noarch/
+	ssh mytestbed.net createrepo /var/www/packages/rpm/5.4/f14
+	ssh mytestbed.net createrepo /var/www/packages/rpm/5.4/f8
 fi
