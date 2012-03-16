65aab3bdb8902af8d70740a9b83d57de87be17d1
Jack C Hong <jhong@nicta.com.au>
2012-03-16 15:37:14 +1100

    Add interfaces to app configure method

--- omf_rc/lib/omf_rc/resource_proxy/app.rb
+++ omf_rc/lib/omf_rc/resource_proxy/app.rb
@@ -2,6 +2,7 @@ module OmfRc
   module ResourceProxy
     module App
       APPINFO = 'dpkg -l'
+      APPTOOL = 'apt'
       def request_property(property)
         case property
         when /^version$/
@@ -12,6 +13,17 @@ module OmfRc
       end
 
       def configure_property(property, value)
+        case property
+        when /^install$/
+          `LANGUAGE='C' LANG='C' LC_ALL='C' DEBIAN_FRONTEND='noninteractive' #{PKGTOOL} install --reinstall --allow-unauthenticated -qq #{uid}`
+        when /^remove$/
+          operation = value == 'purge' ? 'purge' : 'remove'
+          `LANGUAGE='C' LANG='C' LC_ALL='C' DEBIAN_FRONTEND='noninteractive' #{PKGTOOL} #{operation} --allow-unauthenticated -qq #{uid}`
+        when /^exectue$/
+          # TODO build the actual command and execute
+        when /^kill$/
+          # TODO find the running process and send kill signals, value could be the type of KILL signal
+        end
       end
     end
   end
