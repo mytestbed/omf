2aaace89adc8f1cc84ca2e6d47035aff18f47d23
Jack C Hong <jhong@nicta.com.au>
2012-04-30 14:02:23 +1000

    Reduce callback chaos by providing block wrapper to handle errors/results

--- omf_common/lib/omf_common/dsl/xmpp_blather.rb
+++ omf_common/lib/omf_common/dsl/xmpp_blather.rb
@@ -18,23 +18,26 @@ module OmfCommon
           { :var => "pubsub#publish_model", :value => "open" }]
       })
 
+      # Set up XMPP options and start the Eventmachine, connect to XMPP server
+      #
       def connect(username, password, server)
         jid = "#{username}@#{server}"
         client.setup(jid, password)
         client.run
       end
 
+      # Shut down XMPP connection, clean up pubsub nodes if necessary
+      #
+      # @param [String] host Host represents the pubsub address, e.g. pubsub.norbit.npc.nicta.com.au
       def disconnect(host)
-        pubsub.affiliations(host) do |aff|
-          if affiliation[:owner].nil?
+        pubsub.affiliations(host) do |affs|
+          if affs[:owner].nil?
             shutdown
           else
-            affiliation[:owner].each do |item|
-              pubsub.delete(item, host) do |m|
-                logger.error m if m.error?
-                logger.info "PUBSUB NODE DELETED: #{item}"if m.result?
-                pubsub.affiliations(host) do |a|
-                  shutdown if a[:owner].nil?
+            affs[:owner].each do |item|
+              delete_node(item, host) do |m|
+                pubsub.affiliations(host) do |affs_left|
+                  shutdown if affs_left[:owner].nil?
                 end
               end
             end
@@ -42,38 +45,66 @@ module OmfCommon
         end
       end
 
+      # Create a new pubsub node with additional configuration
+      #
+      # @param [String] node Pubsub node name
+      # @param [String] host Pubsub host address
       def create_node(node, host, &block)
-        new_block = proc do |e|
-          logger.error e if e.error?
-          block.call(e) if block
-        end
-        pubsub.create_with_configuration(node, PUBSUB_CONFIGURE, host, &new_block)
+        pubsub.create_with_configuration(node, PUBSUB_CONFIGURE, host, &new_block(__method__, node, &block))
       end
 
+      # Delete a pubsub node
+      #
+      # @param [String] node Pubsub node name
+      # @param [String] host Pubsub host address
       def delete_node(node, host, &block)
-        pusbub.delete(node, host, &block)
+        pubsub.delete(node, host, &new_block(__method__, &block))
       end
 
+      # Subscribe to a pubsub node
+      #
+      # @param [String] node Pubsub node name
+      # @param [String] host Pubsub host address
       def subscribe(node, host, &block)
-        pubsub.subscribe(node, nil, host, &block)
+        pubsub.subscribe(node, nil, host, &new_block(__method__, node, &block))
       end
 
-      def unsubscribe(node, host)
+      # Un-subscribe all existing subscriptions from all pubsub nodes.
+      #
+      # @param [String] node Pubsub node name
+      # @param [String] host Pubsub host address
+      def unsubscribe(host)
         pubsub.subscriptions(host) do |m|
-          m.subscribed.each do |s|
-            pusbub.unsubscribe(node, nil, s.id, host)
+          m[:subscribed] && m[:subscribed].each do |s|
+            pubsub.unsubscribe(s[:node], nil, s[:subid], host, &new_block(__method__, s[:node], s[:subid]))
           end
         end
       end
 
+      # Publish to a pubsub node
+      #
+      # @param [String] node Pubsub node name
+      # @param [String] message Any XML fragment to be sent as payload
+      # @param [String] host Pubsub host address
       def publish(node, message, host, &block)
-        pubsub.publish(node, message, host, &block)
+        pubsub.publish(node, message, host, &new_block(__method__, node, &block))
       end
 
       def node_event(*args, &block)
-        pubsub_event(*args, &block)
+        pubsub_event(*args, &new_block(__method__, &block))
       end
 
+      private
+
+      # Provide a new block wrap to automatically log errors
+      def new_block(*args, &block)
+        m = args.empty? ? "OPERATION" : args.map {|v| v.to_s.upcase }.join(" ")
+        proc do |callback|
+          logger.error callback if callback.respond_to?(:error?) && callback.error?
+          logger.info "#{m} SUCCEED" if callback.respond_to?(:result?) && callback.result?
+          block.call(callback) if block
+        end
+      end
     end
   end
 end
