1bfb143ec04a035e0218096c3c2ee6f6b4bd1835
Max Ott <max.ott@nicta.com.au>
2012-05-26 22:01:39 +1000

    Looks like I didn't add the OML outputted after all - or the wrong one.

--- omf-expctl/ruby/omf-expctl/oml/oml_outputter.rb
+++ omf-expctl/ruby/omf-expctl/oml/oml_outputter.rb
@@ -1,65 +1,41 @@
 require 'rubygems'
-require 'monitor'
-require 'thread'
+require 'oml4r'
 require 'log4r/outputter/outputter'
 
-module OMF; module EC; module Communicator; end end end
+module OMF; module EC; module OML; end end end
 
-module OMF::EC::Communicator
+module OMF::EC::OML
   
-  class PubOutputter < Log4r::Outputter
+  # Send log messages to OML
+  #
+  class OMLOutputter < Log4r::Outputter
     include MonitorMixin
       
-    def initialize(name = 'publog', hash={})
+    def initialize(name = 'omllog', hash={})
       super(name, hash)
-      @queue = Queue.new
-      _publish
     end
-    
-    # Can't allow log level to be set to DEBUG as sending log mesages
-    # does also create new log messages which create even more.
-    #
-    def level=(level)
-      if (level < 2)
-        Logger.log_internal(4) do "PubOutputter: Can't allow log level to be set to DEBUG" end
-        level = 2
-      end
-      super
-    end
-    
+        
     def canonical_log(event)
-      msg ={}
-      msg[:logger] = event.fullname
-      msg[:level] = event.level
-      msg[:level_name] = Log4r::LNAMES[event.level]
-      msg[:data] = event.data
-      if event.tracer
-        msg[:tracer] = event.tracer
-      end
-      @queue << msg 
+      LogMP.inject event.fullname, 
+                    event.level,
+                    Log4r::LNAMES[event.level],
+                    event.data
+                    event.tracer ? event.tracer : '-'
     end
     
-    private 
-    
-    def _publish
-      Thread.new do
-        begin
-          comms = ECCommunicator.instance
-          # The communicator won't be ready when we come here, let's wait for that
-          while ! comms.initialized?
-            sleep 2
-          end
-          
-          while (msg = @queue.pop)
-            comms.send_log_message(msg)
-          end
-        rescue Exception => ex
-          Logger.log_internal(4) do "PubOutputter: while sending log message #{ex}" end
-        end
-      end
-    end
 
-  end # PubOutputter
+  end # OMLOutputter
+  
+  # Define your own Measurement Point
+  class LogMP < OML4R::MPBase
+    name :log
+
+    param :logger
+    param :level, :type => :long
+    param :level_name
+    param :data
+    param :tracer
+  end
   
 end # module
   
