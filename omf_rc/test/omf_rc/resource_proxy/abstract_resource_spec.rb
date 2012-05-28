820e71f960f8837d7c7135680b66b6653551e97a
Jack C Hong <jhong@nicta.com.au>
2012-05-28 15:37:39 +1000

    Test additional helper methods in abstract resource

--- omf_rc/test/omf_rc/resource_proxy/abstract_resource_spec.rb
+++ omf_rc/test/omf_rc/resource_proxy/abstract_resource_spec.rb
@@ -30,18 +30,17 @@ end
 
 describe AbstractResource do
   before do
-    @node = OmfRc::ResourceFactory.new(:node, properties: { pubsub: 'mytestbed.net' })
+    @node = OmfRc::ResourceFactory.new(:node, { hrn: 'default_node' })
   end
 
   describe "when intialised" do
     it "must convert configuration hash into instance methods, and assign the values" do
       @node.type.must_equal 'node'
-      @node.properties.must_be_kind_of Hash
-      @node.properties.pubsub.must_equal "mytestbed.net"
     end
 
     it "must have an unique id generated" do
       @node.uid.must_match /.{8}-.{4}-.{4}-.{4}-.{12}/
+      @node.request_uid.must_match /.{8}-.{4}-.{4}-.{4}-.{12}/
     end
   end
 
@@ -51,14 +50,17 @@ describe AbstractResource do
     end
 
     it "must add the resource to its created resource list" do
-      child = @node.create(:wifi)
+      child = @node.create(:wifi, { hrn: 'default_wifi' })
       @node.children.must_include child
+      @node.request_child_resources[child.uid].must_equal 'default_wifi'
     end
   end
 
   describe "when destroyed" do
     it "must destroy itself together with any resources created by it" do
-      skip
+      @node.release
+      @node.frozen?.must_equal true
+      @node.children.must_be_empty
     end
   end
 
@@ -68,6 +70,13 @@ describe AbstractResource do
       @node.request_available_properties.configure.must_include :name
       @node.request_available_properties.request.must_include :name
     end
+
+    it "must be able to request and configure some common properties" do
+      @node.request_hrn.must_equal 'default_node'
+      @node.configure_hrn('bob')
+      @node.request_hrn.must_equal 'bob'
+    end
   end
 end
+
 
