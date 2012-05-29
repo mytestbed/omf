4d05e1ef410a6ca4d85dfc9d6270520212c0a627
Jack C Hong <jhong@nicta.com.au>
2012-05-29 12:20:32 +1000

    Documenting resource proxy dsl...

--- omf_rc/lib/omf_rc/resource_proxy_dsl.rb
+++ omf_rc/lib/omf_rc/resource_proxy_dsl.rb
@@ -7,17 +7,66 @@ module OmfRc::ResourceProxyDSL
   end
 
   module ClassMethods
+    # Register a named proxy entry with factory class, normally this should be done in the proxy module
+    #
+    # @param [Symbol] name of the resource proxy
+    # @example suppose we define a module for wifi
+    #
+    #   module OmfRc::ResourceProxy::Wifi
+    #     include OmfRc::ResourceProxyDSL
+    #
+    #     # Let the factory know it is available
+    #     register_proxy :wifi
+    #   end
+    #
     def register_proxy(name)
       name = name.to_sym
       OmfRc::ResourceFactory.register_proxy(name)
     end
 
+    # Register some hooks which can be called at certain stage of the operation
+    #
+    # Currently the system supports two hooks:
+    #
+    # * before_ready, called when a resource created, before creating an associated pubsub node
+    # * before_release, called before a resource released
+    #
+    # @param [Symbol] name hook name. :before_create or :before_release
+    # @yieldparam [AbstractResource] resource pass the current resource object to the block
+    # @example
+    #
+    #   module OmfRc::ResourceProxy::Node
+    #    include OmfRc::ResourceProxyDSL
+    #
+    #    register_proxy :node
+    #
+    #    register_hook :before_ready do |resource|
+    #      logger.info "#{resource.uid} is now ready"
+    #    end
+    #
+    #    register_hook :before_release do |resource|
+    #      logger.info "#{resource.uid} is now released"
+    #    end
+    #   end
     def register_hook(name, &register_block)
       define_method(name) do
         register_block.call(self) if register_block
       end
     end
 
+    # Include the utility by providing a name
+    #
+    # The utility file can be added to the default utility directory UTIL_DIR, or defined inline.
+    #
+    # @param [Symbol] name of the utility
+    # @example assume we have a module called iw.rb in the omf_rc/util directory, providing a module named OmfRc::Util::Iw with functionalities based on iw cli
+    #
+    #   module OmfRc::ResourceProxy::Wifi
+    #     include OmfRc::ResourceProxyDSL
+    #
+    #     # Simply include this util module
+    #     utility :iw
+    #   end
     def utility(name)
       name = name.to_s
       begin
@@ -36,17 +85,75 @@ module OmfRc::ResourceProxyDSL
       end
     end
 
+    # Register a named utility entry with factory class, normally this should be done in the utility module
+    #
+    # @param [Symbol] name of the resource proxy
+    # @example suppose we define a utility for iw command interaction
+    #
+    #   module OmfRc::Util::Iw
+    #     include OmfRc::ResourceProxyDSL
+    #
+    #     # Let the factory know it is available
+    #     register_utility :iw
+    #   end
     def register_utility(name)
       name = name.to_sym
       OmfRc::ResourceFactory.register_utility(name)
     end
 
+    # Register a configurable property
+    #
+    # @param [Symbol] name of the property
+    # @yieldparam [AbstractResource] resource pass the current resource object to the block
+    # @yieldparam [Object] value pass the value to be configured
+    # @example suppose we define a utility for iw command interaction
+    #
+    #   module OmfRc::Util::Iw
+    #     include OmfRc::ResourceProxyDSL
+    #
+    #     register_configure :freq do |resource, value|
+    #       Command.execute("iw #{resource.hrn} set freq #{value}")
+    #     end
+    #
+    #     # or use iterator to define multiple properties
+    #     %w(freq channel type).each do |p|
+    #       register_configure p do |resource, value|
+    #         Command.execute("iw #{resource.hrn} set freq #{value}")
+    #       end
+    #     end
+    #
+    #     # or we can try to parse iw's help page to extract valid properties and then automatically register them
+    #     Command.execute("iw help").chomp.gsub(/^\t/, '').split("\n").map {|v| v.match(/[phy|dev] <.+> set (\w+) .*/) && $1 }.compact.uniq.each do |p|
+    #       register_configure p do |resource, value|
+    #         Command.execute("iw #{resource.hrn} set #{p} #{value}")
+    #       end
+    #     end
+    #   end
     def register_configure(name, &register_block)
       define_method("configure_#{name.to_s}") do |*args, &block|
-        register_block.call(self, *args, block) if register_block
+      register_block.call(self, *args, block) if register_block
       end
     end
 
+    # Register a property that could be requested
+    #
+    # @param (see #register_configure)
+    # @yieldparam [AbstractResource] resource pass the current resource object to the block
+    # @example suppose we define a utility for iw command interaction
+    #   module OmfRc::Util::Iw
+    #     include OmfRc::ResourceProxyDSL
+    #
+    #     register_request :freq do |resource|
+    #       Command.execute("iw #{resource.hrn} link").match(/^(freq):\W*(.+)$/) && $2
+    #     end
+    #
+    #     # or we can grab everything from output of iw link command and return as a hash(mash)
+    #     Command.execute("iw #{resource.hrn} link").chomp.gsub(/^\t/, '').split("\n").drop(1).each do |v|
+    #       v.match(/^(.+):\W*(.+)$/).tap do |m|
+    #         m && known_properties[m[1].downcase.gsub(/\W+/, '_')] = m[2].gsub(/^\W+/, '')
+    #       end
+    #     end
+    #   end
     def register_request(name, &register_block)
       define_method("request_#{name.to_s}") do |*args|
         register_block.call(self) if register_block
--- omf_rc/lib/omf_rc/util/iw.rb
+++ omf_rc/lib/omf_rc/util/iw.rb
@@ -2,9 +2,7 @@ require 'hashie'
 module OmfRc::Util::Iw
   include OmfRc::ResourceProxyDSL
 
-  IW_CMD = "iw"
-
-  OmfCommon::Command.execute("#{IW_CMD} help").chomp.gsub(/^\t/, '').split("\n").map {|v| v.match(/[phy|dev] <.+> set (\w+) .*/) && $1 }.compact.uniq.each do |p|
+  OmfCommon::Command.execute("iw help").chomp.gsub(/^\t/, '').split("\n").map {|v| v.match(/[phy|dev] <.+> set (\w+) .*/) && $1 }.compact.uniq.each do |p|
     register_configure p do |resource, value|
       OmfCommon::Command.execute("#{IW_CMD} #{resource.hrn} set #{p} #{value}")
     end
@@ -13,7 +11,7 @@ module OmfRc::Util::Iw
   register_request :link do |resource|
     known_properties = Hashie::Mash.new
 
-    OmfCommon::Command.execute("#{IW_CMD} #{resource.hrn} link").chomp.gsub(/^\t/, '').split("\n").drop(1).each do |v|
+    OmfCommon::Command.execute("iw #{resource.hrn} link").chomp.gsub(/^\t/, '').split("\n").drop(1).each do |v|
       v.match(/^(.+):\W*(.+)$/).tap do |m|
         m && known_properties[m[1].downcase.gsub(/\W+/, '_')] = m[2].gsub(/^\W+/, '')
       end
