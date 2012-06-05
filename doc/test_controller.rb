85aebed5bc532d8f56d9407fd4a5cbfcfbb96827
Jack C Hong <jhong@nicta.com.au>
2012-06-05 18:26:32 +1000

    Annoate engine test controller

--- doc/test_controller.rb
+++ doc/test_controller.rb
@@ -8,24 +8,28 @@ include OmfCommon
 options = {
   user: 'bravo',
   password: 'pw',
-  server: 'localhost',
-  uid: 'mclaren',
-  pubsub_host: 'pubsub',
+  server: 'localhost', # XMPP pubsub server domain
+  uid: 'mclaren', # The garage's name, we used the same name in the garage_controller.
+  pubsub_host: 'pubsub'  # The host name of pubsub system
 }
 
+# We will use Comm directly, with default DSL implementaion :xmpp_blather
 comm = Comm.new(:xmpp_blather)
 host = nil
 
-# For simplicity, use comm instance directly
+# Then we can register event handlers to the communicator
+#
+# Event triggered when connection is ready
 comm.when_ready do
   logger.info "CONNECTED: #{comm.jid.inspect}"
   host = "#{options[:pubsub_host]}.#{comm.jid.domain}"
 
-  # We assume the node where RC runs started already
+  # We assume that a garage resource proxy instance is up already, so we subscribe to its pubsub node
   comm.subscribe(options[:uid], host) do |e|
     if e.error?
       comm.disconnect(host)
     else
+      # If subscribed, we publish a 'create' message, 'create' a new engine for testing
       comm.publish(
         options[:uid],
         Message.create { |v| v.property('type', 'engine') },
@@ -34,7 +38,7 @@ comm.when_ready do
   end
 end
 
-# Fired when messages published to the nodes I subscribed to
+# Triggered when new messages published to the nodes I subscribed to
 comm.node_event do |e|
   e.items.each do |item|
     begin
@@ -49,45 +53,41 @@ comm.node_event do |e|
           logger.info "Engine #{engine_id} ready for testing"
 
           comm.subscribe(engine_id, host) do
-            # Engine is ready, get some information about the engine
-            comm.publish(
-              engine_id,
-              Message.request do |v|
-                v.property('max_rpm')
-                v.property('provider')
-                v.property('max_power')
-              end.sign,
-              host
-            )
-
-            # Checking RPM every 1 second
-            EM.add_periodic_timer(1) do
-              comm.publish(engine_id,
-                           Message.request { |v| v.property('rpm') }.sign,
-                           host)
-            end
-
-            # Apply 50% throttle
+            # Now engine is ready, we can ask for some information about the engine
             comm.publish(engine_id,
-                         Message.configure { |v| v.property('throttle', '50') }.sign,
+                         Message.request do |v|
+                           v.property('max_rpm')
+                           v.property('provider')
+                           v.property('max_power')
+                         end.sign,
                          host)
 
-            # Some time we want to reduce throttle to 0, avoid blowing up the engine
-            EM.add_timer(5) do
-              comm.publish(engine_id,
-                           Message.configure { |v| v.property('throttle', '0') }.sign,
-                           host)
+            # We will check engine's RPM every 1 second
+            EM.add_periodic_timer(1) do
+             comm.publish(engine_id,
+                          Message.request { |v| v.property('rpm') }.sign,
+                          host)
             end
 
-            # 30 seconds later, we will 'release' this engine
+            # Now we will apply 50% throttle to the engine
+            comm.publish(engine_id,
+                        Message.configure { |v| v.property('throttle', '50') }.sign,
+                        host)
 
+            # Some time later, we want to reduce the throttle to 0, to avoid blowing up the engine
+            EM.add_timer(5) do
+             comm.publish(engine_id,
+                          Message.configure { |v| v.property('throttle', '0') }.sign,
+                          host)
+            end
+
+            # 20 seconds later, we will 'release' this engine, i.e. shut it down
             EM.add_timer(20) do
-              comm.publish(engine_id,
-                           Message.release.sign,
-                           host)
+             comm.publish(engine_id,
+                          Message.release.sign,
+                          host)
             end
           end
-
         when 'STATUS'
           message.read_element("//property").each do |p|
             logger.info "#{p.attr('key')} => #{p.content.strip}"
@@ -109,4 +109,3 @@ EM.run do
   trap(:INT) { comm.disconnect(host) }
   trap(:TERM) { comm.disconnect(host) }
 end
-
