4fd87b84d1df6e22634371a28cb6e8bce6cb2ec1
Jack C Hong <jhong@nicta.com.au>
2012-06-19 14:12:13 +1000

    No need for register dsl

--- omf_common/lib/omf_common/core_ext/blather/dsl.rb
+++
@@ -1,8 +0,0 @@
-module Blather
-  module DSL
-    def register(username, password, &block)
-      stanza = Blather::Stanza::Registration.new(username, password)
-      client.write_with_handler(stanza, &block)
-    end
-  end
-end
