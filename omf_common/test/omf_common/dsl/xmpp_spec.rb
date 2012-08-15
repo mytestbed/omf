7e236a124a37f495f8dbb75f9b9eed7e726e327e
Jack C Hong <jhong@nicta.com.au>
2012-08-15 16:27:29 +1000

    Allow message generation to be part of communicator

---
+++ omf_common/test/omf_common/dsl/xmpp_spec.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+include OmfCommon::DSL::Xmpp
+
+describe OmfCommon::DSL::Xmpp do
+  describe "when omf message related methods" do
+    it "must generate omf create xml fragment" do
+      m1 = generate_create_message([type: 'engine'])
+      m2 = generate_create_message do |v|
+        v.property('type', 'test')
+      end
+      m1.must_equal m2
+      m1.name.must_equal 'create'
+      m1.to_xml.must_match /<property key="type">engine<\/property>/
+    end
+
+    it "must generate omf configure xml fragment" do
+      m1 = generate_configure_message([throttle: 50])
+      m2 = generate_configure_message do |v|
+        v.property('throttle', 50)
+      end
+      m1.must_equal m2
+      m1.name.must_equal 'configure'
+      m1.to_xml.must_match /<property key="throttle">50<\/property>/
+    end
+
+    it "must generate omf inform xml fragment" do
+      m1 = generate_inform_message([inform_type: 'CREATED'])
+      m2 = generate_inform_message do |v|
+        v.property('inform_type', 'test')
+      end
+      m1.must_equal m2
+      m1.name.must_equal 'inform'
+      m1.to_xml.must_match /<property key="inform_type">CREATED<\/property>/
+    end
+
+    it "must generate omf release xml fragment" do
+      m1 = generate_release_message([resource_id: 100])
+      m2 = generate_release_message do |v|
+        v.property('resource_id', 100)
+      end
+      m1.must_equal m2
+      m1.name.must_equal 'release'
+      m1.to_xml.must_match /<property key="resource_id">100<\/property>/
+    end
+
+    it "must generate omf request xml fragment" do
+      m1 = generate_request_message([:max_rpm, {:provider => {country: 'japan'}}, :max_power])
+      m2 = generate_request_message do |v|
+        v.property('max_rpm')
+        v.property('provider') do |p|
+          p.element('country', 'japan')
+        end
+        v.property('max_power')
+      end
+      m1.must_equal m2
+      m1.name.must_equal 'request'
+      m1.to_xml.must_match /<property key="max_rpm"\/>/
+      m1.to_xml.must_match /<property key="provider">/
+      m1.to_xml.must_match /<country>japan<\/country>/
+      m1.to_xml.must_match /<property key="max_power"\/>/
+    end
+  end
+end
+
