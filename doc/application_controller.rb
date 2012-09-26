31f1c50fdc7ea99887dc0065989ee4dce075f69a
Thierry Rakotoarivelo <thierry.rakotoarivelo@nicta.com.au>
2012-09-26 17:13:44 +1000

    Add doc and example for the generic application proxy

---
+++ doc/application_controller.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'omf_rc'
+require 'omf_rc/resource_factory'
+require 'omf_rc/resource_proxy/generic_application.rb'
+$stdout.sync = true
+
+options = {
+  user: 'app_proxy_test',
+  password: '123',
+  server: 'localhost', # Pubsub server domain
+  uid: 'app_test', # Id of the resource
+}
+
+EM.run do
+  # Use resource factory method to initialise a new instance of the resource
+  my_application = OmfRc::ResourceFactory.new(:generic_application, options)
+  # Let the resource to XMPP server
+  my_application.connect
+
+  # Disconnect the resource from Pubsub server, when these two signals received
+  trap(:INT) { my_application.disconnect }
+  trap(:TERM) { my_application.disconnect }
+end
