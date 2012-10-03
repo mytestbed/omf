3539cb7f3d3f7e0897ab5af3912b429353c01a5b
Jack C Hong <jhong@nicta.com.au>
2012-10-03 14:24:47 +1000

    Modify engine tutorial test script using script loader

--- omf_ec/example/engine_test.rb
+++ omf_ec/example/engine_test.rb
@@ -1,40 +1,21 @@
-#!/usr/bin/env ruby
+# OMF_VERSIONS = 6.0
 
-require 'omf_common'
-$stdout.sync = true
-
-include OmfCommon
-
-options = {
-  user: 'bravo',
-  password: 'pw',
-  server: 'localhost', # XMPP pubsub server domain
-  uid: 'mclaren', # The garage's name, we used the same name in the garage_controller.
-  debug: true
-}
-
-garage_id = options[:uid]
-
-Logging.logger.root.level = options[:debug] ? :debug : :info
-Blather.logger = logger
-
-# We will use Comm directly, with default DSL implementaion :xmpp
-comm = Comm.new(:xmpp)
-
-garage_topic = comm.get_topic(garage_id)
+# @comm is default communicator defined in script runner
+#
+garage_id = "mclaren"
+garage_topic = @comm.get_topic(garage_id)
 
 garage_topic.on_message proc { |m| m.operation != :inform } do |message|
-  logger.warn message
+  logger.info message
 end
 
-# messages { key: Topic }
 msgs = {
-  create: comm.create_message([type: 'engine']),
-  request: comm.request_message([:max_rpm, {:provider => {country: 'japan'}}, :max_power]),
-  request_rpm: comm.request_message([:rpm]),
-  increase_throttle: comm.configure_message([throttle: 50]),
-  reduce_throttle: comm.configure_message([throttle: 0]),
-  test_error_handling: comm.request_message([:error]),
+  create: @comm.create_message([type: 'mp4']),
+  request: @comm.request_message([:max_rpm, {:provider => {country: 'japan'}}, :max_power]),
+  request_rpm: @comm.request_message([:rpm]),
+  increase_throttle: @comm.configure_message([throttle: 50]),
+  reduce_throttle: @comm.configure_message([throttle: 0]),
+  test_error_handling: @comm.request_message([:error]),
 }
 
 msgs[:test_error_handling].on_inform_failed do |message|
@@ -64,10 +45,10 @@ end
 
 # Triggered when new messages published to the topics I subscribed to
 msgs[:create].on_inform_created do |message|
-  engine_topic = comm.get_topic(message.resource_id)
+  engine_topic = @comm.get_topic(message.resource_id)
   engine_id = engine_topic.id
 
-  msgs[:release] ||= comm.release_message { |m| m.element('resource_id', engine_id) }
+  msgs[:release] ||= @comm.release_message { |m| m.element('resource_id', engine_id) }
 
   msgs[:release].on_inform_released  do |message|
     logger.info "Engine (#{message.resource_id}) turned off (resource released)"
@@ -85,7 +66,7 @@ msgs[:create].on_inform_created do |message|
     # Now we will apply 50% throttle to the engine
     msgs[:increase_throttle].publish engine_id
 
-    comm.add_timer(5) do
+    @comm.add_timer(5) do
       # Some time later, we want to reduce the throttle to 0, to avoid blowing up the engine
       msgs[:reduce_throttle].publish engine_id
 
@@ -94,7 +75,7 @@ msgs[:create].on_inform_created do |message|
     end
 
     # 10 seconds later, we will 'release' this engine, i.e. shut it down
-    comm.add_timer(10) do
+    @comm.add_timer(10) do
       msgs[:release].publish garage_id
     end
   end
@@ -103,18 +84,12 @@ end
 # Then we can register event handlers to the communicator
 #
 # Event triggered when connection is ready
-comm.when_ready do
-  logger.info "CONNECTED: #{comm.jid.inspect}"
+@comm.when_ready do
+  logger.info "CONNECTED: #{@comm.jid.inspect}"
 
   # We assume that a garage resource proxy instance is up already, so we subscribe to its pubsub topic
   garage_topic.subscribe do
     # If subscribed, we publish a 'create' message, 'create' a new engine for testing
     msgs[:create].publish garage_topic.id
   end
-end
-
-EM.run do
-  comm.connect(options[:user], options[:password], options[:server])
-  trap(:INT) { comm.disconnect }
-  trap(:TERM) { comm.disconnect }
 end
