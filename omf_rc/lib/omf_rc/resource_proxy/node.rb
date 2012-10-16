ce266f0bc23f2d4153486aaa2ebb697ef18c99e5
Jack C Hong <jhong@nicta.com.au>
2012-10-16 14:59:25 +1100

    Avoid using chdir in multi-thread environment

--- omf_rc/lib/omf_rc/resource_proxy/node.rb
+++ omf_rc/lib/omf_rc/resource_proxy/node.rb
@@ -23,33 +23,31 @@ module OmfRc::ResourceProxy::Node
 
   request :devices do |resource|
     devices = []
-    Dir.chdir("/sys/class") do
-      # Support net devices for now
-      category = "net"
+    # Support net devices for now
+    category = "net"
 
-      Dir.glob("net/eth*").each do |v|
-        File.exist?("#{v}/uevent") && File.open("#{v}/uevent") do |f|
-          subcategory = f.read.match(/DEVTYPE=(.+)/) && $1
-          proxy = "net"
-          File.exist?("#{v}/device/uevent") && File.open("#{v}/device/uevent") do |f|
-            driver = f.read.match(/DRIVER=(.+)/) && $1
-            device = { name: File.basename(v), driver: driver, category: category }
-            device[:subcategory] = subcategory if subcategory
-            device[:proxy] = proxy if resource.request_proxies.include?(proxy.to_sym)
-            devices << device
-          end
-        end
-      end
-
-      Dir.glob("ieee80211/*").each do |v|
-        subcategory = "wlan"
-        proxy = "wlan"
+    Dir.glob("/sys/class/net/eth*").each do |v|
+      File.exist?("#{v}/uevent") && File.open("#{v}/uevent") do |f|
+        subcategory = f.read.match(/DEVTYPE=(.+)/) && $1
+        proxy = "net"
         File.exist?("#{v}/device/uevent") && File.open("#{v}/device/uevent") do |f|
           driver = f.read.match(/DRIVER=(.+)/) && $1
-          device = { name: File.basename(v), driver: driver, category: category, subcategory: subcategory }
+          device = { name: File.basename(v), driver: driver, category: category }
+          device[:subcategory] = subcategory if subcategory
           device[:proxy] = proxy if resource.request_proxies.include?(proxy.to_sym)
           devices << device
         end
+      end
+    end
+
+    Dir.glob("/sys/class/ieee80211/*").each do |v|
+      subcategory = "wlan"
+      proxy = "wlan"
+      File.exist?("#{v}/device/uevent") && File.open("#{v}/device/uevent") do |f|
+        driver = f.read.match(/DRIVER=(.+)/) && $1
+        device = { name: File.basename(v), driver: driver, category: category, subcategory: subcategory }
+        device[:proxy] = proxy if resource.request_proxies.include?(proxy.to_sym)
+        devices << device
       end
     end
     devices
