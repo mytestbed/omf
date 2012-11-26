453e6af853331ac7ad5a0337db5b3a5e4f2e15c5
Jack C Hong <jhong@nicta.com.au>
2012-11-26 13:04:14 +1100

    Add wlan_phy_devices shortcut method

--- omf_rc/lib/omf_rc/util/sysfs.rb
+++ omf_rc/lib/omf_rc/util/sysfs.rb
@@ -14,7 +14,7 @@ module OmfRc::Util::Sysfs
           driver = f.read.match(/DRIVER=(.+)/) && $1
           device = { name: File.basename(v), driver: driver, category: category }
           device[:subcategory] = subcategory if subcategory
-          device[:proxy] = proxy if resource.request_proxies.include?(proxy.to_sym)
+          device[:proxy] = proxy if OmfRc::ResourceFactory.proxy_list.include?(proxy.to_sym)
           devices << device
         end
       end
@@ -26,11 +26,15 @@ module OmfRc::Util::Sysfs
       File.exist?("#{v}/device/uevent") && File.open("#{v}/device/uevent") do |f|
         driver = f.read.match(/DRIVER=(.+)/) && $1
         device = { name: File.basename(v), driver: driver, category: category, subcategory: subcategory }
-        device[:proxy] = proxy if resource.request_proxies.include?(proxy.to_sym)
+        device[:proxy] = proxy if OmfRc::ResourceFactory.proxy_list.include?(proxy.to_sym)
         devices << device
       end
     end
     devices
+  end
+
+  request :wlan_devices do |resource|
+    resource.request_devices.find_all { |v| v[:proxy] == 'wlan' }
   end
 end
 
