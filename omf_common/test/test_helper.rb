c56b37aed5a64e571d1eaf9d594fe118b14ab781
Jack C Hong <jhong@nicta.com.au>
2013-02-04 15:53:01 +1100

    Change tools binary to execution mode

--- omf_common/test/test_helper.rb
+++ omf_common/test/test_helper.rb
@@ -7,14 +7,6 @@ require 'minitest/pride'
 
 require 'omf_common'
 
-#OmfCommon::Comm.init(type: :xmpp)
-
-#OmfCommon.init(:development, {
-#  debug: true,
-#  communication: { type: :xmpp, server: 'localhost' },
-#  eventloop: { type: :em }
-#})
-
 OmfCommon::Message.init(type: :xml)
 
 # Shut up all the loggers
