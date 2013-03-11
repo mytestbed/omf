6b4be52bc60644053d8c2729d9e732d930f0b827
Christoph Dwertmann <christoph.dwertmann@nicta.com.au>
2013-03-11 16:04:00 +1100

    added ruby version check, updated install guide

--- doc/oedl_simple_test.rb
+++ doc/oedl_simple_test.rb
@@ -5,14 +5,17 @@ defProperty('res1', "unconfigured-node-1", "ID of a node")
 defGroup('Actor', property.res1)
 
 onEvent(:ALL_UP) do |event|
+  # 'after' is not blocking. This executes 3 seconds after :ALL_UP fired.
   after 3 do
     info "TEST - allGroups"
     allGroups.exec("/bin/date")
   end
+  # 'after' is not blocking. This executes 6 seconds after :ALL_UP fired.  
   after 6 do
     info "TEST - group"
     group("Actor").exec("/bin/hostname -f")
   end
+  # 'after' is not blocking. This executes 9 seconds after :ALL_UP fired.  
   after 9 do
     Experiment.done
   end
