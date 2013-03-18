58aeb80f91ae02522cbe0b8b89be4276bd40a2c1
Jack C Hong <jhong@nicta.com.au>
2013-03-18 16:51:40 +1100

    Bump pre versions

--- omf_common/lib/omf_common/version.rb
+++ omf_common/lib/omf_common/version.rb
@@ -1,4 +1,4 @@
 module OmfCommon
-  VERSION = "6.0.0.pre.10"
+  VERSION = "6.0.0.pre.11"
   PROTOCOL_VERSION = "6.0"
 end
--- omf_rc/lib/omf_rc/version.rb
+++ omf_rc/lib/omf_rc/version.rb
@@ -1,3 +1,3 @@
 module OmfRc
-  VERSION = "6.0.0.pre.8"
+  VERSION = "6.0.0.pre.9"
 end
