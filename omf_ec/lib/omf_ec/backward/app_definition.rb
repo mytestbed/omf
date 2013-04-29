e8c4b32c1271bebcc1e66506a219b8fd5ef12764
Olivier Mehani <olivier.mehani@nicta.com.au>
2013-04-29 17:30:25 +1000

    Properly document oml2-scaffold options for OmfEc::Backward::AppDefinition
    fixes: #1168 @1h
    refs: #1159
    
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- omf_ec/lib/omf_ec/backward/app_definition.rb
+++ omf_ec/lib/omf_ec/backward/app_definition.rb
@@ -7,12 +7,27 @@ module OmfEc
       # This method is for backward compatibility with previous OEDL 5.
       #
       # @param [String] name name of the property to define (mandatory)
-      # @param [String] description description of this property
+      # @param [String] description description of this property; oml2-scaffold uses this for the help message (popt: descrip)
       # @param [String] parameter command-line parameter to introduce this property, including dashes if needed (can be nil)
       # @param [Hash] options list of options associated with this property
-      # @option options [String] :type type of the property: :integer, :string and :boolean are supported
+      # @option options [String] :type type of the property: :integer, :string and :boolean are supported; oml2-scaffold extends this with :int and :double (popt: argInfo)
       # @option options [Boolean] :dynamic true if the property can be changed at run-time
       # @option options [Fixnum] :order used to order properties when creating the command line
+      #
+      # The OML code-generation tool, oml2-scaffold extends the range of
+      # options supported in the options hash to support generation of
+      # popt(3) command line parsing code. As for the parameters, depending
+      # on the number of dashes (two/one) in parameter, it is used as the
+      # longName/shortName for popt(3), otherwise the former defaults to
+      # name, and the latter defaults to either :mnemonic or nothing.
+      #
+      # @option options [String] :mnemonic one-letter mnemonic for the option (also returned by poptGetNextOpt as val)
+      # @option options [String] :unit unit in which this property is expressed; oml2-scaffold uses this for the help message (popt: argDescrip)
+      # @option options [String] :default default value if argument unspecified (optional; defaults to something sane for the :type)
+      # @option options [String] :var_name name of the C variable for popt(3) to store the property value into (optional; popt: arg; defaults to name, after sanitisation)
+      #
+      # @see http://oml.mytestbed.net/doc/oml/latest/oml2-scaffold.1.html
+      # @see http://linux.die.net/man/3/popt
       #
       def defProperty(name = :mandatory, description = nil, parameter = nil, options = {})
         opts = {:description => description, :cmd => parameter}
