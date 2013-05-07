f4b035a5f2fdf23d91b29fb5d18c06fd7dd0fd30
Max Ott <max.ott@nicta.com.au>
2013-05-07 21:46:55 +1000

    Small demo program to visualise the garage example. Really needs a README file and a Rakefile wouldn't hurt as well.

---
+++ omf_common/example/viz/garage_monitor.rb
@@ -0,0 +1,64 @@
+require 'omf_oml/table'
+
+class GarageMonitor < OMF::Common::LObject
+  OP_MODE = :development
+
+  def initialize(opts)
+    @observed_topics = {}
+
+    create_event_table
+    Thread.new do
+      begin
+        sleep 3
+        OmfCommon.init(OP_MODE, opts) do |el|
+          OmfCommon.comm.on_connected do |comm|
+            observe('garage1', comm)
+          end
+        end
+      rescue Exception => ex
+        puts "ERROR: #{ex}"
+        puts "\t#{ex.backtrace.join("\n\t")}"
+      end
+    end
+    
+    
+  end
+  
+  def observe(tname, comm)
+    return if (tname.nil? || @observed_topics.key?(tname))
+  
+    info "Observing '#{tname}'"
+    @observed_topics[tname] = true
+    comm.subscribe(tname) do |topic|
+      topic.on_message do |msg|
+        ts = Time.now.strftime('%H:%M:%S')
+        src = topic.id
+        type = "#{msg.type}(#{msg.itype})"
+        msg.each_property do |name, value|
+          @evt_table.add_row [ts, src, type, name, value]
+        end
+        if msg.itype == 'released' && src != :garage1
+          @engine_table.add_row [ts, src, -1, -1]
+        end
+        
+        if (msg[:throttle] && msg[:rpm])
+          puts "RPM: #{msg[:rpm]}::#{msg[:rpm].class}"
+          @engine_table.add_row [ts, src, msg[:throttle], msg[:rpm]]
+        end
+        observe(msg[:res_id], comm)
+      end
+    end
+  end
+  
+  
+  
+  def create_event_table(fake_events = true)
+    evt_schema = [:ts, :address, :type, :name, :value]
+    @evt_table = OMF::OML::OmlTable.new 'events', evt_schema, :max_size => 500
+    OMF::Web.register_datasource @evt_table
+
+    engine_schema = [:ts, :source, [:throttle, :int], [:rpm, :int]]
+    @engine_table = OMF::OML::OmlTable.new 'engine', engine_schema, :max_size => 20
+    OMF::Web.register_datasource @engine_table.indexed_by(:source), name: 'engine'
+  end
+end
---
+++ omf_common/example/viz/garage_viz.rb
@@ -0,0 +1,47 @@
+
+require 'omf_common'
+require 'omf_common/lobject'
+# require 'yaml'
+# require 'log4r'
+
+OMF::Common::Loggable.init_log 'garage_monitor', searchPath: File.dirname(__FILE__)
+
+
+omf_opts = {
+  communication: {
+    url: 'amqp://0.0.0.0',
+    auth: {}
+
+  },
+  eventloop: { type: :em},
+  logging: {
+    level: 'info'
+  }
+}
+
+# Configure the web server
+#
+opts = {
+  app_name: 'garage_monitor',
+  page_title: 'Garage',
+  layout: "#{File.dirname(__FILE__)}/layout.yaml",
+  footer_right: 'git:mytestbed/omf',
+  static_dirs_pre: ["#{File.dirname(__FILE__)}/htdocs"],
+  handlers: {
+    # delay connecting to databases to AFTER we may run as daemon
+    pre_rackup: lambda do
+      load("#{File.dirname(__FILE__)}/garage_monitor.rb")
+      GarageMonitor.new(omf_opts)
+    end,
+    pre_parse: lambda do |p|
+      p.separator ""
+      p.separator "GARAGE options:"
+      p.on '--comms-url URL', "URL to communication layer [#{omf_opts[:communication][:url]}]" do |url|
+        omf_opts[:communication][:url] = url
+      end
+      p.separator ""
+    end
+  }
+}
+require 'omf_web'
+OMF::Web.start(opts)
