838835e9a28bca20fc859435fb1ee40420699867
Jack C Hong <jhong@nicta.com.au>
2013-05-21 14:36:03 +1000

    Add license text to the top of every ruby file
    refs #975

--- omf_common/lib/omf_common/key.rb
+++ omf_common/lib/omf_common/key.rb
@@ -1,3 +1,8 @@
+# Copyright (c) 2012 National ICT Australia Limited (NICTA).
+# This software may be used and distributed solely under the terms of the MIT license (License).
+# You should find a copy of the License in LICENSE.TXT or at http://opensource.org/licenses/MIT.
+# By downloading or using this software you accept the terms and the liability disclaimer in the License.
+
 require 'openssl'
 require 'singleton'
 
