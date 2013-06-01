fe30b0b11fd653853efd173237c6832587c60888
Jack C Hong <jhong@nicta.com.au>
2013-06-01 14:51:43 +1000

    Fix broken rc test

--- omf_rc/test/omf_rc/resource_factory_spec.rb
+++ omf_rc/test/omf_rc/resource_factory_spec.rb
@@ -15,17 +15,14 @@ describe OmfRc::ResourceFactory do
 
     it "must have list of registered proxies and utilities" do
       OmfRc::ResourceFactory.load_default_resource_proxies
-      OmfRc::ResourceFactory.proxy_list.must_include :mock
+      OmfRc::ResourceFactory.proxy_list.must_include :node
     end
 
     it "must be able to create new resource proxy" do
       OmfCommon.stub :comm, @xmpp do
         OmfRc::ResourceFactory.load_default_resource_proxies
-        mock = OmfRc::ResourceFactory.create(:mock)
-        mock.must_be_kind_of OmfRc::ResourceProxy::AbstractResource
-        mock.must_respond_to :request_nothing
-        mock.request_nothing.must_equal mock.uid
-        mock.must_respond_to :configure_nothing
+        node = OmfRc::ResourceFactory.create(:node)
+        node.must_be_kind_of OmfRc::ResourceProxy::AbstractResource
       end
     end
 
