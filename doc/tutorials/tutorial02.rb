fa0e162abba8d0c32114f4a0f500b1ddef5edae1
mitchmusarra <mitch@dhcp-0-133.visitor.nicta.net>
2013-06-19 14:37:32 +1000

    Changed comments
    Comments are more relevant to code

--- doc/tutorials/tutorial02.rb
+++ doc/tutorials/tutorial02.rb
@@ -1,33 +1,9 @@
-#Welcome to 'Hello World' Wireless
-#This script creates a simple wireless networking experiment
+#Welcome to 'Hello World' Wired
+#This script creates a simple wired networking experiment
 
 
 #Section 1
-#Define otr2 application file-paths
-#Define experiment parameters and measurement points
-
-defApplication('otr2') do |a|
-    
-    
-	#Application description and binary path
-    a.binary_path = "/usr/bin/otr2"
-    a.description = "OTG is a configurable traffic generator. It contains generators producing various forms of packet streams and port for sending these packets via various transports, such as TCP and UDP. This version 2 is compatible with OMLv2"
-    
-    #Define configurable parameters of otr2
-    a.defProperty('udp_local_host', 'IP address of this Destination node', '--udp:local_host', {:type => :string, :dynamic => false})
-    a.defProperty('udp_local_port', 'Receiving Port of this Destination node', '--udp:local_port', {:type => :integer, :dynamic => false})
-    a.defMeasurement('udp_in') do |m|
-        m.defMetric('ts',:float)
-        m.defMetric('flow_id',:long)
-        m.defMetric('seq_no',:long)
-        m.defMetric('pkt_length',:long)
-        m.defMetric('dst_host',:string)
-        m.defMetric('dst_port',:long)
-    end
-end
-
-
-#Define otg2 application file-paths
+#Define otg2 application
 #Define experiment parameters and measurement points
 defApplication('otg2') do |a|
     
@@ -54,7 +30,6 @@ defApplication('otg2') do |a|
         m.defMetric('ts',:float)
         m.defMetric('flow_id',:long)
         m.defMetric('seq_no',:long)
-        m.defMetric('pkt_length',:long)
         m.defMetric('dst_host',:string)
         m.defMetric('dst_port',:long)
         
@@ -62,59 +37,72 @@ defApplication('otg2') do |a|
 end
 
 
+#Define otr2 application file-paths
+#Define experiment parameters and measurement points
+defApplication('otr2') do |a|
+        
+    #Application description and binary path
+    a.binary_path = "/usr/bin/otr2"
+    a.description = "OTG is a configurable traffic generator. It contains generators producing various forms of packet streams and port for sending these packets via various transports, such as TCP and UDP. This version 2 is compatible with OMLv2"
+    
+    #Define configurable parameters of otr2
+    a.defProperty('udp_local_host', 'IP address of this Destination node', '--udp:local_host', {:type => :string, :dynamic => false})
+    a.defProperty('udp_local_port', 'Receiving Port of this Destination node', '--udp:local_port', {:type => :integer, :dynamic => false})
+    a.defMeasurement('udp_in') do |m|
+        m.defMetric('ts',:float)
+        m.defMetric('flow_id',:long)
+        m.defMetric('seq_no',:long)
+        m.defMetric('pkt_length',:long)
+        m.defMetric('dst_host',:string)
+        m.defMetric('dst_port',:long)
+
+    end
+end
+
+    
 
 #Section 2
-#Define resources and nodes used by application
+#Define recources and nodes used by application
 
-#Define configuration of wireless 'sender'
-defGroup('Sender', "omf.nicta.node36") do |node|
+#Define configuration of wired 'sender'
+defGroup('Sender', "omf.nicta.node9") do |node|
     node.addApplication("otg2") do |app|
         app.setProperty('udp_local_host', '192.168.0.2')
-        app.setProperty('udp_dst_host', '192.168.0.3')
-        app.setProperty('udp_dst_port', 3000)
-        app.measure('udp_out', :interval => 3)
-    end
+            app.setProperty('udp_dst_host', '192.168.0.3')
+            app.setProperty('udp_dst_port', 3000)
+            app.measure('udp_out', :samples => 1)
+        end
     
-    node.net.w0.mode = "adhoc"
-    node.net.w0.type = 'g'
-    node.net.w0.channel = "6"
-    node.net.w0.essid = "Hello World!"
-    node.net.w0.ip = "192.168.0.2/24"
+    node.net.e0.ip = "192.168.0.2/24"
 end
 
 
-#Define configuration of wireless 'reciever'
-defGroup('Receiver', "omf.nicta.node37") do |node|
+#Define configuration of wired 'reciever'
+defGroup('Receiver', "omf.nicta.node10") do |node|
     node.addApplication("otr2") do |app|
         app.setProperty('udp_local_host', '192.168.0.3')
-        app.setProperty('udp_local_port', 3000)
-        app.measure('udp_in', :interval => 3)
-    end
+            app.setProperty('udp_local_port', 3000)
+            app.measure('udp_in', :samples => 1)
+        end
     
-    node.net.w0.mode = "adhoc"
-    node.net.w0.type = 'g'
-    node.net.w0.channel = "6"
-    node.net.w0.essid = "Hello World!"
-    node.net.w0.ip = "192.168.0.3/24"
+    node.net.e0.ip = "192.168.0.3/24"
 end
 
 
-
-
-#Section 3
+#Section  3
 #Execution of application events
-
 onEvent(:ALL_UP_AND_INSTALLED) do |event|
-    info "Starting WiFi OMF6 Experiment events"
+    info "Starting OMF6 Experiment events"
     
-    after 10 do
+    
+    after 10
         allGroups.startApplications
         info "All Applications have started..."
-        
     end
-    after 40 do
+    
+    after 30
         allGroups.stopApplications
         info "Applications are stopping... Experiment Complete."
         Experiment.done
-    end
-end
+
+
