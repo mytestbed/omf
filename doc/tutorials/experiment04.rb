1cdae5854f19c915e7907856a8f3660e3c4fbfb8
mitchmusarra <mitch@dhcp-0-181.visitor.nicta.net>
2013-06-20 13:55:01 +1000

    Theoretically working code (w/ one exception)
    Exception of 'allGroups.net.w0'
    'net' option not implemented in OMF6

--- doc/tutorials/experiment04.rb
+++ doc/tutorials/experiment04.rb
@@ -1,14 +1,19 @@
-defApplication('otr2') do |a|
-    
+#Welcome to Experiment 04
+#This script allows experimenters to configure or address all resources within a defined group ane use simple substitutions
+
+#Section 1
+#Define otr2 application file-paths
+#Define experiment parameters and measurement points
+defApplication('otr2') do |app|
     
 	#Application description and binary path
-    a.binary_path = "/usr/bin/otr2"
-    a.description = "otr is a configurable traffic sink that recieves packet streams"
+    app.binary_path = "/usr/bin/otr2"
+    app.description = "otr is a configurable traffic sink that recieves packet streams"
     
     #Define configurable parameters of otr2
-    a.defProperty('udp_local_host', 'IP address of this Destination node', '--udp:local_host', {:type => :string, :dynamic => false})
-    a.defProperty('udp_local_port', 'Receiving Port of this Destination node', '--udp:local_port', {:type => :integer, :dynamic => false})
-    a.defMeasurement('udp_in') do |m|
+    app.defProperty('udp_local_host', 'IP address of this Destination node', '--udp:local_host', {:type => :string, :dynamic => false})
+    app.defProperty('udp_local_port', 'Receiving Port of this Destination node', '--udp:local_port', {:type => :integer, :dynamic => false})
+    app.defMeasurement('udp_in') do |m|
         m.defMetric('ts',:float)
         m.defMetric('flow_id',:long)
         m.defMetric('seq_no',:long)
@@ -18,31 +23,30 @@ defApplication('otr2') do |a|
     end
 end
 
-
 #Define otg2 application file-paths
 #Define experiment parameters and measurement points
-defApplication('otg2') do |a|
+defApplication('otg2') do |app|
     
     #Application description and binary path
-    a.binary_path = "/usr/bin/otg2"
-    a.description = "otg is a configurable traffic generator that sends packet streams"
+    app.binary_path = "/usr/bin/otg2"
+    app.description = "otg is a configurable traffic generator that sends packet streams"
     
     #Define configurable parameters of otg2
-    a.defProperty('generator', 'Type of packet generator to use (cbr or expo)', '-g', {:type => :string, :dynamic => false})
-    a.defProperty('udp_broadcast', 'Broadcast', '--udp:broadcast', {:type => :integer, :dynamic => false})
-    a.defProperty('udp_dst_host', 'IP address of the Destination', '--udp:dst_host', {:type => :string, :dynamic => false})
-    a.defProperty('udp_dst_port', 'Destination Port to send to', '--udp:dst_port', {:type => :integer, :dynamic => false})
-    a.defProperty('udp_local_host', 'IP address of this Source node', '--udp:local_host', {:type => :string, :dynamic => false})
-    a.defProperty('udp_local_port', 'Local Port of this source node', '--udp:local_port', {:type => :integer, :dynamic => false})
-    a.defProperty("cbr_size", "Size of packet [bytes]", '--cbr:size', {:dynamic => true, :type => :integer})
-    a.defProperty("cbr_rate", "Data rate of the flow [kbps]", '--cbr:rate', {:dynamic => true, :type => :integer})
-    a.defProperty("exp_size", "Size of packet [bytes]", '--exp:size', {:dynamic => true, :type => :integer})
-    a.defProperty("exp_rate", "Data rate of the flow [kbps]", '--exp:rate', {:dynamic => true, :type => :integer})
-    a.defProperty("exp_ontime", "Average length of burst [msec]", '--exp:ontime', {:dynamic => true, :type => :integer})
-    a.defProperty("exp_offtime", "Average length of idle time [msec]", '--exp:offtime', {:dynamic => true, :type => :integer})
+    app.defProperty('generator', 'Type of packet generator to use (cbr or expo)', '-g', {:type => :string, :dynamic => false})
+    app.defProperty('udp_broadcast', 'Broadcast', '--udp:broadcast', {:type => :integer, :dynamic => false})
+    app.defProperty('udp_dst_host', 'IP address of the Destination', '--udp:dst_host', {:type => :string, :dynamic => false})
+    app.defProperty('udp_dst_port', 'Destination Port to send to', '--udp:dst_port', {:type => :integer, :dynamic => false})
+    app.defProperty('udp_local_host', 'IP address of this Source node', '--udp:local_host', {:type => :string, :dynamic => false})
+    app.defProperty('udp_local_port', 'Local Port of this source node', '--udp:local_port', {:type => :integer, :dynamic => false})
+    app.defProperty("cbr_size", "Size of packet [bytes]", '--cbr:size', {:dynamic => true, :type => :integer})
+    app.defProperty("cbr_rate", "Data rate of the flow [kbps]", '--cbr:rate', {:dynamic => true, :type => :integer})
+    app.defProperty("exp_size", "Size of packet [bytes]", '--exp:size', {:dynamic => true, :type => :integer})
+    app.defProperty("exp_rate", "Data rate of the flow [kbps]", '--exp:rate', {:dynamic => true, :type => :integer})
+    app.defProperty("exp_ontime", "Average length of burst [msec]", '--exp:ontime', {:dynamic => true, :type => :integer})
+    app.defProperty("exp_offtime", "Average length of idle time [msec]", '--exp:offtime', {:dynamic => true, :type => :integer})
     
     #Define measurement points that application will output
-    a.defMeasurement('udp_out') do |m|
+    app.defMeasurement('udp_out') do |m|
         m.defMetric('ts',:float)
         m.defMetric('flow_id',:long)
         m.defMetric('seq_no',:long)
@@ -53,6 +57,8 @@ defApplication('otg2') do |a|
     end
 end
 
+#Section 2
+#Define resources and nodes used by application
 defGroup('Sender', "omf.nicta.node9") do |node|
     node.addApplication("otg2") do |app|
         app.setProperty('udp_local_host', '%net.w0.ip%')
@@ -71,14 +77,17 @@ defGroup('Receiver', "omf.nicta.node10,omf.nicta.node11") do |node|
     end
 end
 
+#Currently not working in OMF6
 allGroups.net.w0 do |interface|
     interface.mode = "adhoc"
     interface.type = 'g'
     interface.channel = "6"
-    interface.essid = "helloworld"
+    interface.essid = "Hello World! Experiment04"
     interface.ip = "192.168.0.%index%"
 end
 
+#Section 3
+#Execution of application events
 onEvent(:ALL_UP_AND_INSTALLED) do |event|
     after 10
     group("Receiver").startApplications
