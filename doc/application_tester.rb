aa00199b20fb186fd9e7bfb9463cf35ebe791a41
Jack C Hong <jhong@nicta.com.au>
2013-07-31 13:17:31 +1000

    Update how to use app proxy tutorial

--- doc/application_tester.rb
+++ doc/application_tester.rb
@@ -3,101 +3,52 @@
 require 'omf_common'
 $stdout.sync = true
 
-include OmfCommon
-
-options = {
-  user: 'app_proxy_tester',
-  password: '123',
-  server: 'localhost', # Pubsub pubsub server domain
-  uid: 'app_test' # Name of the application resource (see applicaiton_controller.rb)
-}
-
-app_id = options[:uid]
-
-comm = Comm.new(:xmpp)
-
-# Ask our communicator to give us the topic to which the Application
-# Proxy is subscribed
-app_topic = comm.get_topic(app_id)
-
-# For any 'inform' message posted on that topic...
-# if it is an event from the application, log it as information
-# if it is an error/warning from the Application Proxy, log it accordingly
-app_topic.on_message  do |m|
-  if m.operation == :inform
-    case m.read_content("itype")
+def run_test(app)
+  # Set up inform message handler to print inform messages
+  app.on_inform do |m|
+    case m.itype
     when 'STATUS'
-      if m.read_property("status_type") == 'APP_EVENT'
-        logger.info "APP_EVENT #{m.read_property('event')} "+
-        "from app #{m.read_property("app")} - msg: #{m.read_property("msg")}"
+      if m[:status_type] == 'APP_EVENT'
+        info "APP_EVENT #{m[:event]} from app #{m[:app]} - msg: #{m[:msg]}"
       end
     when 'ERROR'
-      logger.error m.read_content('reason') if m.read_content("itype") == 'ERROR'
+      error m[:reason]
     when 'WARN'
-      logger.warn m.read_content('reason') if m.read_content("itype") == 'WARN'
+      warn m[:reason]
     end
   end
-end
 
-# Here we construct the different messages that we will publish later when we
-# will interact with the Application proxy
-msgs = {
-  # request the OS platform on which the App Proxy is running
-  req_platform: comm.request_message([:platform]),
-  # configure the 'binary_path' property of the App Proxy
-  conf_path: comm.configure_message([binary_path: "/bin/ping"]),
-  # configure the available parameters for the application handled by the App Proxy
-  conf_parameters: comm.configure_message([parameters: {
-    :timestamp => {:type => 'Boolean', :cmd => '-D', :mandatory => false},
-    :target => {:type => 'String', :cmd => '', :mandatory => true, :default => 'localhost', :order => 2},
-    :count => {:type => 'Numeric', :cmd => '-c', :mandatory => true, :default =>3, :order => 1},
-  }]),
-  # update the value of some parameters
-  update_param: comm.configure_message([parameters: {
-    :target => {:value => 'nicta.com.au'},
-    :timestamp => {:value => true}
-  }]),
-  # ask the App Proxy to run the application
-  run_application: comm.configure_message([state: :run])
-}
+  # Configure the 'binary_path' and 'parameters' properties of the App Proxy
+  app.configure(binary_path: "/bin/ping",
+                parameters: { :target => { :value => 'nicta.com.au' }})
 
-# Register a block of commands to handle all 'inform' messages
-# published as replies to our 'req_platform'
-msgs[:req_platform].on_inform_status do |m|
-  m.each_property do |p|
-    logger.info "#{p.attr('key')} => #{p.content.strip}"
+  # Start the application 2 seconds later
+  OmfCommon.eventloop.after 2 do
+    app.configure(state: :running)
+  end
+
+  # Stop the application another 10 seconds later
+  OmfCommon.eventloop.after 12 do
+    app.configure(state: :stopped)
   end
 end
 
+OmfCommon.init(:development,
+               communication: { url: 'xmpp://localhost' },
+               logging: { level: { default: 'info' } }) do
+  OmfCommon.comm.on_connected do |comm|
+    info "Test application >> Connected to XMPP as #{comm.conn_info}"
 
-# Then we can register event handlers to the communicator
-#
-# Event triggered when connection is ready
-comm.when_ready do
-  logger.info "CONNECTED: #{comm.jid.inspect}"
+    # Subscribe to the proxy topic
+    comm.subscribe('app_test') do |app|
+      if app.error?
+        error app.inspect
+      else
+        # Now subscribed, run the test
+        run_test(app)
+      end
+    end
 
-  # We assume that a application resource proxy instance is up already,
-  # so we subscribe to its pubsub topic
-  app_topic.subscribe do
-    # If subscribed, we start publishing messages some messages
-    # to interact with our Application Proxy
-    msgs[:req_platform].publish app_topic.id
-    sleep 1
-    msgs[:conf_path].publish app_topic.id
-    sleep 1
-    msgs[:conf_parameters].publish app_topic.id
-    sleep 1
-    msgs[:run_application].publish app_topic.id
-    sleep 2
-    msgs[:update_param].publish app_topic.id
-    sleep 1
-    msgs[:run_application].publish app_topic.id
-    sleep 2
+    comm.on_interrupted { comm.disconnect }
   end
-end
-
-EM.run do
-  comm.connect(options[:user], options[:password], options[:server])
-  trap(:INT) { comm.disconnect }
-  trap(:TERM) { comm.disconnect }
 end
