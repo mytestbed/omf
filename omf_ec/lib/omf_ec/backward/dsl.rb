bd4e5ee1012ea801b737dfd45ffd33ae0f9e724b
Christoph Dwertmann <christoph.dwertmann@nicta.com.au>
2013-10-03 06:11:56 +0000

    Fixes #1482

--- omf_ec/lib/omf_ec/backward/dsl.rb
+++ omf_ec/lib/omf_ec/backward/dsl.rb
@@ -50,7 +50,7 @@ module OmfEc
       def wait(duration)
         info "Request from Experiment Script: Wait for #{duration}s...."
         warn "Calling 'wait' or 'sleep' will block entire EC event loop. Please try 'after' or 'every'"
-        sleep duration
+        sleep duration.to_s.to_i
       end
     end
   end
