23cb236ddc30f75a24459805a283fff00fb0f3b4
Max Ott <max.ott@nicta.com.au>
2013-12-06 10:58:42 +1100

    Added debug message to see what actually is requested to be created

--- omf_rc/lib/omf_rc/resource_factory.rb
+++ omf_rc/lib/omf_rc/resource_factory.rb
@@ -27,6 +27,7 @@ class OmfRc::ResourceFactory
         raise ArgumentError, "Resource type not found: #{type.to_s}" unless @@proxy_list.include?(type)
       end
       # Create a new instance of abstract resource and return it
+      debug "Create resource type: #{type} opts: #{opts} copts: #{creation_opts}"
       OmfRc::ResourceProxy::AbstractResource.new(type, opts, creation_opts) do |res|
         creation_callback.call(res) if creation_callback
       end
