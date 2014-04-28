5b00790da1adfdfc63e84bb356690ae157de6453
Jack C Hong <jhong@nicta.com.au>
2014-04-28 13:39:46 +1000

    Adding wifi state reporting to wlan RP

--- omf_rc/lib/omf_rc/resource_proxy/wlan.rb
+++ omf_rc/lib/omf_rc/resource_proxy/wlan.rb
@@ -41,22 +41,34 @@ module OmfRc::ResourceProxy::Wlan
   #   @!macro prop
   property :phy
 
+  property :timer
+
   # @!endgroup
 
   # @!macro group_hook
-  #
+  hook :before_ready do |device|
+    device.property.timer = OmfCommon.el.every(5) do |timer|
+      wlan_state = device.request_state
+      if wlan_state == 'UP'
+        device.inform(:status, { state: wlan_state })
+        timer.cancel
+      end
+    end
+  end
+
   # Stop hostapd or wpa instances before releasing wifi device
   # @!macro hook
   # @!method before_release
   hook :before_release do |device|
+    device.property.timer.cancel
+
     case device.property.mode.to_sym
     when :master
       device.stop_hostapd
     when :managed
       device.stop_wpa
     end
-    #TODO need to remove all virtual interfaces of that phy device
-    #device.remove_all_interfaces
+    device.interface_down
   end
   # @!endgroup
 end
