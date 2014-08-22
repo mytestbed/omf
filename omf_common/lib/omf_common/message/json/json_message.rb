9116086df7e19c80efdb6176c8e6cdae97aff555
Jack C Hong <jack.hong@nicta.com.au>
2014-08-22 15:30:01 +1000

    Typo fix in json_message.rb

--- omf_common/lib/omf_common/message/json/json_message.rb
+++ omf_common/lib/omf_common/message/json/json_message.rb
@@ -93,7 +93,7 @@ module OmfCommon
               warn "JWT: Message is missing :iss element"
               return nil
             end
-            if ceat_pem = claims[:crt]
+            if cert_pem = claims[:crt]
               # let's the credential store take care of it
               pem = "#{OmfCommon::Auth::Certificate::BEGIN_CERT}#{cert_pem}#{OmfCommon::Auth::Certificate::END_CERT}"
               OmfCommon::Auth::CertificateStore.instance.register_x509(pem)
